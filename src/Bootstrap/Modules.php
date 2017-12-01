487c5d9d7f28f5886e1ebf006ecfb10d88325e84
Julien Turbide <jturbide@nuagerie.com>
2017-12-01 18:32:19 -0500

    repo init composer

---
+++ src/Bootstrap/Modules.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Zemit\Bootstrap;
+
+use Phalcon\Application;
+
+/**
+ * Require a config from the DI
+ * Config : [modules => ['api', 'backend', 'frontend']]
+ *
+ * Class Modules
+ * @package Zemit\Bootstrap
+ */
+class Modules {
+    
+    public function __construct(Application $application)
+    {
+        /**
+         * Register application modules
+         */
+        $config = $application->getDI()->get('config');
+        if (!empty($config)) {
+            $registerModules = array();
+            foreach ($config->modules as $module) {
+                $registerModules[$module] = array(
+                    'className' => 'Zemit\\' . ucfirst($module) . '\\Module',
+                    'path' => __DIR__ . '/../modules/' . $module . '/Module.php'
+                );
+            }
+            if (!empty($registerModules)) {
+                $application->registerModules($registerModules);
+            }
+            else {
+                // @TODO maybe throw an error
+            }
+        }
+        else {
+            // @TODO maybe throw an error
+        }
+        
+    }
+    
+}
