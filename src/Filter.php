b1b7d7cb93a70487b54d41448e53abd5c54e27b0
Julien Turbide <jturbide@nuagerie.com>
2018-03-25 11:18:54 -0400

    fix filters

--- src/Filter.php
+++ src/Filter.php
@@ -6,14 +6,17 @@ use Phalcon\Filter as PhalconFilter;
 
 class Filter extends PhalconFilter
 {
-    const FILTER_JSON = "json";
-    const FILTER_SERIALIZE = "serialize";
+    const FILTER_MD5 = 'md5';
+    const FILTER_JSON = 'json';
+    const FILTER_IPV4 = 'ipv4';
+    const FILTER_IPV6 = 'ipv6';
     
     public function __construct()
     {
+        $this->add(self::FILTER_MD5, new Filters\Md5());
         $this->add(self::FILTER_JSON, new Filters\Json());
-        $this->add(self::FILTER_SERIALIZE, new Filters\Serialize());
-        $this->add(self::FILTER_FRONTEND, new Filters\Frontend());
+        $this->add(self::FILTER_IPV4, new Filters\IPv4());
+        $this->add(self::FILTER_IPV6, new Filters\IPv6());
     }
     
 }
