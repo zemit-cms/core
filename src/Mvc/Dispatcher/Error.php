b35e9d535c10baea8a404e9c6b208619d3c606d2
Julien Turbide <jturbide@nuagerie.com>
2020-03-28 23:24:14 -0400

    fix error handling & dispatcher error

--- src/Mvc/Dispatcher/Error.php
+++ src/Mvc/Dispatcher/Error.php
@@ -26,30 +26,40 @@ class Error extends Injectable
         switch ($exception->getCode()) {
             case Dispatcher\Exception::EXCEPTION_NO_DI:
                 // no di, calm down for now and see what happens after
+                // @todo why?
                 break;
             case Dispatcher\Exception::EXCEPTION_HANDLER_NOT_FOUND:
             case Dispatcher\Exception::EXCEPTION_ACTION_NOT_FOUND:
+                
                 // Not found, forward to errors->notFound (404)
-                $dispatcher->forward([
-                    'controller' => 'errors',
-                    'action' => 'notFound',
-                    'params' => [
-                        'exception' => $exception
-                    ]
-                ]);
-                return false;
-                break;
-            default:
-                // Everything else, ff debug is false, forward forward to fatal error 500
-                if (!$this->config->app->debug) {
+                if ($dispatcher->getControllerName() !== 'errors' && $dispatcher->getActionName() !== 'notFound') {
                     $dispatcher->forward([
                         'controller' => 'errors',
-                        'action' => 'fatal',
+                        'action' => 'notFound',
                         'params' => [
                             'exception' => $exception
                         ]
                     ]);
+                } else {
+                    throw new \Exception($exception->getMessage(), $exception->getCode(), $exception);
+                }
+                return false;
+                break;
+            default:
+                // Everything else, if debug is false, forward forward to fatal error 500
+                if (!$this->config->app->debug && !$this->config->debug->enable) {
+                    if ($dispatcher->getControllerName() !== 'errors' && $dispatcher->getActionName() !== 'fatal') {
+                        $dispatcher->forward([
+                            'controller' => 'errors',
+                            'action' => 'fatal',
+                            'params' => [
+                                'exception' => $exception
+                            ]
+                        ]);
+                    }
                     return false;
+                } else {
+                    throw new \Exception($exception->getMessage(), $exception->getCode(), $exception);
                 }
                 break;
         }
