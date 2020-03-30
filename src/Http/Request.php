da275f23793b5fc81514cc722c83d37c33beca26
Julien Turbide <jturbide@nuagerie.com>
2020-03-30 00:05:42 -0400

    new zemit request provider with toArray

---
+++ src/Http/Request.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Http;
+
+/**
+ * Class
+ * {@inheritdoc} \Phalcon\Http\Request
+ * @package Zemit\Http
+ */
+class Request extends \Phalcon\Http\Request
+{
+    public function toArray() {
+        $config = $this->getDI()->get('config');
+        if ($config->app->debug || $config->debug->enable) {
+            return [
+                'body' => $this->getRawBody(),
+                'post' => $this->getPost(),
+                'get' => $this->get(),
+                'put' => $this->getPut(),
+                'headers' => $this->getHeaders(),
+                'userAgent' => $this->getUserAgent(),
+                'basicAuth' => $this->getBasicAuth(),
+                'bestAccept' => $this->getBestAccept(),
+                'bestCharset' => $this->getBestCharset(),
+                'bestLanguage' => $this->getBestLanguage(),
+                'clientAddress' => $this->getClientAddress(),
+                'clientCharsets' => $this->getClientCharsets(),
+                'contentType' => $this->getContentType(),
+                'digestAuth' => $this->getDigestAuth(),
+                'httpHost' => $this->getHttpHost(),
+                'uri' => $this->getURI(),
+                'serverName' => $this->getServerName(),
+                'serverAddress' => $this->getServerAddress(),
+                'method' => $this->getMethod(),
+                'port' => $this->getPort(),
+                'httpReferer' => $this->getHTTPReferer(),
+                'languages' => $this->getLanguages(),
+                'scheme' => $this->getScheme(),
+                'isAjax' => $this->isAjax(),
+                'isGet' => $this->isGet(),
+                'isDelete' => $this->isDelete(),
+                'isHead' => $this->isHead(),
+                'isPatch' => $this->isPatch(),
+                'isConnect' => $this->isConnect(),
+                'isTrace' => $this->isTrace(),
+                'isPut' => $this->isPut(),
+                'isPurge' => $this->isPurge(),
+                'isOptions' => $this->isOptions(),
+                'isSoap' => $this->isSoap(),
+                'isSecure' => $this->isSecure(),
+                'isValidHttpMethod' => $this->isValidHttpMethod($this->getMethod()),
+            ];
+        }
+        return false;
+    }
+}
---
+++ src/Provider/Request/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Provider\Request;
+
+use Phalcon\Di\DiInterface;
+use Zemit\Http\Request;
+use Zemit\Provider\AbstractServiceProvider;
+
+/**
+ * Zemit\Provider\Request\ServiceProvider
+ *
+ * @package Zemit\Provider\Request
+ */
+class ServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * The Service name.
+     * @var string
+     */
+    protected $serviceName = 'request';
+    
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    public function register(DiInterface $di): void
+    {
+        $di->setShared($this->getName(), function() use ($di) {
+            $request = new Request();
+            $request->setDI($di);
+            return $request;
+        });
+    }
+}
