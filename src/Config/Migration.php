967b9abdce3269080cd2ae2fda19e0d62d0b2d58
Julien Turbide <moi@jturbide.com>
2022-05-16 12:55:39 -0400

    set default database driver during migration remove readonly config during migration fixes to work properly with phalcon devtool

--- src/Config/Migration.php
+++ src/Config/Migration.php
@@ -30,29 +30,10 @@ class Migration extends Config
      */
     public function __construct($config = [])
     {
-        parent::__construct($config);
-        $this->fixDatabaseForPhalconScripts();
-    }
-    
-    /**
-     * Fix the current config to be supported by native Phalcon scripts
-     */
-    public function fixDatabaseForPhalconScripts() {
-        if ($this->has('database.default')) {
-            $default = $this->get('database.default');
-        
-            if ($this->has('database.drivers.' . $default)) {
-                $database = $this->get('database.drivers' . $default);
-            
-                $this->set('database', $database);
-            }
-        }
-    
-        $this->remove('database.options');
-    
-        // @todo test and remove this part
-//        $this->database = $this->database->drivers->{$this->database->default};
-//        unset($this->database->options);
+        $config = parent::__construct($config);
+        $this->database = $this->database->drivers->{$this->database->default};
+        unset($this->database->options);
+        unset($this->database->readOnly);
     }
 }
 
