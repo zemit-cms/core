edf1bfe1dd6e950a153a07e08f3ba9bd23a2b956
Julien Turbide <moi@jturbide.com>
2022-06-21 12:01:01 -0400

    cms page validation

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -10,6 +10,7 @@
 
 namespace Zemit\Models;
 
+use Phalcon\Validation\Validator\Between;
 use Zemit\Models\Base\AbstractPage;
 use Phalcon\Validation\Validator\PresenceOf;
 use Phalcon\Validation\Validator\StringLength\Max;
@@ -32,8 +33,30 @@ class Page extends AbstractPage
     public function validation()
     {
         $validator = $this->genericValidation();
-
-        // @todo validations
+    
+        $validator->add('siteId', new PresenceOf(['message' => $this->_('required')]));
+        $validator->add('siteId', new Between([
+            'minimum' => 0,
+            'maximum' => self::MAX_UNSIGNED_INT,
+            'message' => $this->_('not an unsigned integer'),
+            'allowEmpty' => false,
+        ]));
+        
+        $validator->add('label', new PresenceOf(['message' => $this->_('required')]));
+    
+        /**
+         * We recommend keeping your headline at under or approximately 60 characters to fit Google results that have a
+         * 600-pixel word limit and avoid truncation. Additionally, here are some other SEO best practices:
+         * Always describe the page's content accurately.
+         */
+        $validator->add('title', new Max(['max' => 100, 'message' => $this->_('length-exceeded')]));
+    
+        /**
+         * Meta descriptions can technically be any length, but Google generally truncates snippets to ~155-160 characters.
+         * It's best to keep meta descriptions long enough that they're sufficiently descriptive,
+         * so we recommend descriptions between 50 and 160 characters.
+         */
+        $validator->add('description', new Max(['max' => 200, 'message' => $this->_('length-exceeded')]));
 
         return $this->validate($validator);
     }
