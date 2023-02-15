98fa6645e5bee8dae54747c78f01733db8540aca
Julien Turbide <moi@jturbide.com>
2023-02-15 16:18:48 -0500

    new default openai service provider

---
+++ src/Provider/OpenAi/ServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace App\Provider\OpenAi;
+
+use Orhanerday\OpenAi\OpenAi;
+use Phalcon\Di\DiInterface;
+use Zemit\Bootstrap\Config;
+use Zemit\Provider\AbstractServiceProvider;
+
+/**
+ * Zemit\Provider\OpenAi\ServiceProvider
+ *
+ * @package Zemit\Provider\OpenAi
+ */
+class ServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * The Service name.
+     * @var string
+     */
+    protected $serviceName = 'openai';
+
+    /**
+     * {@inheritdoc}
+     *
+     * Register the Flash Service with the Twitter Bootstrap classes.
+     *
+     * @return void
+     */
+    public function register(DiInterface $di): void
+    {
+        $di->setShared($this->getName(), function() use ($di) {
+            /** @var Config $config */
+            $config = $di->get('config');
+
+            return new OpenAi($config->path('openai.secretKey'));
+        });
+    }
+}
