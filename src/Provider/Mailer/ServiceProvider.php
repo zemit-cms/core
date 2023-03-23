ae4fd7e4d217eea0ce835d41ab942ba5d0db592c
Julien Turbide <moi@jturbide.com>
2023-03-23 13:19:58 -0400

    mailer format

--- src/Provider/Mailer/ServiceProvider.php
+++ src/Provider/Mailer/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -10,82 +11,37 @@
 
 namespace Zemit\Provider\Mailer;
 
-use Phalcon\Config;
+use Phalcon\Config\ConfigInterface;
 use Phalcon\Di\DiInterface;
+use Phalcon\Events\ManagerInterface;
 use Phalcon\Mailer\Manager;
-use InvalidArgumentException;
 use Zemit\Provider\AbstractServiceProvider;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Mailer
- */
 class ServiceProvider extends AbstractServiceProvider
 {
-    /**
-     * The Service name.
-     * @var string
-     */
-    protected $serviceName = 'mailer';
+    protected string $serviceName = 'mailer';
     
-    /**
-     * {@inheritdoc}
-     *
-     * @return void
-     */
     public function register(DiInterface $di): void
     {
-        $di->setShared($this->getName(), function() use ($di) {
+        $di->setShared($this->getName(), function () use ($di) {
             
-            /** @var \Phalcon\Config $config */
             $config = $di->get('config');
+            assert($config instanceof ConfigInterface);
             
-            /** @var \Phalcon\Config|string $driver */
-            $driver = $config->path('mailer.driver', 'sendmail');
+            $mailerConfig = $config->get('mailer')->toArray() ?? [];
             
-            /** @var \Phalcon\Events\Manager $eventsManager */
+            $driver = $mailerConfig['driver'] ?? '';
+            $defaultOptions = $mailerConfig['defaults'] ?? [];
+            $driverOptions = $mailerConfig['drivers'][$driver] ?? [];
+            $options = array_merge($defaultOptions, $driverOptions);
+    
+            $manager = new Manager($options);
+            $manager->setDI($di);
+            
             $eventsManager = $di->get('eventsManager');
-            
-            switch($driver) {
-                case 'smtp':
-                case 'mail':
-                case 'sendmail':
-                    // Get the mailer manager settings
-                    $settings = $config->path('mailer.drivers.' . $driver, []);
-                    $settings = $settings instanceof Config? $settings->toArray() : $settings;
-                    
-                    // Get the mailer manager default settings
-                    $default = $config->path('mailer.default', []);
-                    $default = $default instanceof Config? $default->toArray() : $default;
-                    
-                    // Merge default settings
-                    $settings = array_merge($settings, $default);
-                    
-                    // Prepare the mailer manager
-                    $manager = new Manager($settings);
-                    
-                    // Bind the DI
-                    $manager->setDI($di);
-                    
-                    // Bind the global event manager
-                    $manager->setEventsManager($eventsManager);
-                    
-                    return $manager;
+            if ($eventsManager instanceof ManagerInterface) {
+                $manager->setEventsManager($eventsManager);
             }
-            
-            throw new InvalidArgumentException(
-                sprintf(
-                    'Invalid mail driver. Expected either "smtp" or "mail" or "sendmail". Got "%s".',
-                    is_scalar($driver) ? $driver : var_export($driver, true)
-                )
-            );
         });
     }
 }
