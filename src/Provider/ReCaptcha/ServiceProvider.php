5d9539e182e1085d3ac297e1e020e9f26c2fb9d8
Julien Turbide <moi@jturbide.com>
2023-03-23 17:40:22 -0400

    new reCaptcha

---
+++ src/Provider/ReCaptcha/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Provider\ReCaptcha;
+
+use Phalcon\Di\DiInterface;
+use Zemit\Config\ConfigInterface;
+use Zemit\Provider\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    protected string $serviceName = 'recaptcha';
+    
+    public function register(DiInterface $di): void
+    {
+        $di->setShared($this->getName(), function () use ($di) {
+            
+            $config = $di->get('config');
+            assert($config instanceof ConfigInterface);
+            
+            $options = $config->pathToArray('reCaptcha', []);
+            
+            $secret = $options['secret'] ?? null;
+            $requestMethod = $options['requestMethod'] ?? null;
+            
+            $reCaptcha = new \ReCaptcha\ReCaptcha($secret, $requestMethod);
+            $reCaptcha->setExpectedHostname($options['expectedHostname'] ?? '');
+            $reCaptcha->setExpectedApkPackageName($options['expectedApkPackageName'] ?? '');
+            $reCaptcha->setExpectedAction($options['expectedAction'] ?? '');
+            $reCaptcha->setScoreThreshold($options['scoreThreshold'] ?? 0.5);
+            
+            return $reCaptcha;
+        });
+    }
+}
