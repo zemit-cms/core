b4762fe2ddaff14b44903e13683b73e36e44fe96
Julien Turbide <moi@jturbide.com>
2023-03-23 01:00:02 -0400

    statuscode fix

---
+++ src/Mvc/Controller/Errors.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Mvc\Controller;
+
+use Phalcon\Http\ResponseInterface;
+
+/**
+ * Default Error Actions
+ */
+trait Errors
+{
+    abstract public function setStatusCode(int $code, ?string $message = null): ResponseInterface;
+    
+    /**
+     * Http Status Code - Generic
+     * error
+     */
+    public function errorAction(?int $code = null, ?string $message = null): void
+    {
+        $this->setStatusCode($code, $message);
+    }
+    
+    /**
+     * Http Status Code 400
+     * bad-request
+     */
+    public function badRequestAction() : void
+    {
+        $this->setStatusCode(400);
+    }
+    
+    /**
+     * Http Status Code 401
+     * unauthorized
+     */
+    public function unauthorizedAction(): void
+    {
+        $this->setStatusCode(401);
+    }
+    
+    /**
+     * Http Status Code 403
+     * forbidden
+     */
+    public function forbiddenAction(): void
+    {
+        $this->setStatusCode(403);
+    }
+    
+    /**
+     * Http Status Code 404
+     * not-found
+     */
+    public function notFoundAction(): void
+    {
+        $this->setStatusCode(404);
+    }
+    
+    /**
+     * Http Status Code 500
+     * fatal
+     */
+    public function fatalAction() : void
+    {
+        $this->setStatusCode(500);
+    }
+    
+    /**
+     * Http Status Code 503
+     * maintenance
+     */
+    public function maintenanceAction() : void
+    {
+        $this->setStatusCode(503);
+    }
+}
