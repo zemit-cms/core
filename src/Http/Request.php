db7a61bd78da25a0d4a2958a2654c35f0282fc6d
Julien Turbide <moi@jturbide.com>
2023-03-24 00:20:14 -0400

    http request refactor

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -11,42 +12,30 @@
 namespace Zemit\Http;
 
 /**
- * Class Request
  * {@inheritDoc}
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Http
  */
 class Request extends \Phalcon\Http\Request
 {
     /**
      * Return true if cors request
-     * @return bool
      */
-    public function isCors() : bool
+    public function isCors(): bool
     {
         return !empty($this->getHeader('Origin')) && !$this->isSameOrigin();
     }
-    
+
     /**
      * Return true if preflight request
-     * @return bool
      */
-    public function isPreflight() : bool
+    public function isPreflight(): bool
     {
         return $this->isCors()
             && $this->isOptions()
             && !empty($this->getHeader('Access-Control-Request-Method'));
     }
-    
+
     /**
      * Return true if the header origin is the same as the request http host
-     * @return bool
      */
     public function isSameOrigin(): bool
     {
@@ -54,52 +43,51 @@ class Request extends \Phalcon\Http\Request
         return $this->getHeader('Origin') === $schemeHost;
     }
     
-    public function toArray()
+    /**
+     * Return a list of the current request attributes
+     */
+    public function toArray(): array
     {
-        $config = $this->getDI()->get('config');
-        if ($config->app->debug || $config->debug->enable) {
-            return [
-                'body' => $this->getRawBody(),
-                'post' => $this->getPost(),
-                'get' => $this->get(),
-                'put' => $this->getPut(),
-                'headers' => $this->getHeaders(),
-                'userAgent' => $this->getUserAgent(),
-                'basicAuth' => $this->getBasicAuth(),
-                'bestAccept' => $this->getBestAccept(),
-                'bestCharset' => $this->getBestCharset(),
-                'bestLanguage' => $this->getBestLanguage(),
-                'clientAddress' => $this->getClientAddress(),
-                'clientCharsets' => $this->getClientCharsets(),
-                'contentType' => $this->getContentType(),
-                'digestAuth' => $this->getDigestAuth(),
-                'httpHost' => $this->getHttpHost(),
-                'uri' => $this->getURI(),
-                'serverName' => $this->getServerName(),
-                'serverAddress' => $this->getServerAddress(),
-                'method' => $this->getMethod(),
-                'port' => $this->getPort(),
-                'httpReferer' => $this->getHTTPReferer(),
-                'languages' => $this->getLanguages(),
-                'scheme' => $this->getScheme(),
-                'isAjax' => $this->isAjax(),
-                'isGet' => $this->isGet(),
-                'isDelete' => $this->isDelete(),
-                'isHead' => $this->isHead(),
-                'isPatch' => $this->isPatch(),
-                'isConnect' => $this->isConnect(),
-                'isTrace' => $this->isTrace(),
-                'isPut' => $this->isPut(),
-                'isPurge' => $this->isPurge(),
-                'isOptions' => $this->isOptions(),
-                'isSoap' => $this->isSoap(),
-                'isSecure' => $this->isSecure(),
-                'isCors' => $this->isCors(),
-                'isPreflight' => $this->isPreflight(),
-                'isSameOrigin' => $this->isSameOrigin(),
-                'isValidHttpMethod' => $this->isValidHttpMethod($this->getMethod()),
-            ];
-        }
-        return false;
+        return [
+            'body' => $this->getRawBody(),
+            'post' => $this->getPost(),
+            'get' => $this->get(),
+            'put' => $this->getPut(),
+            'headers' => $this->getHeaders(),
+            'userAgent' => $this->getUserAgent(),
+            'basicAuth' => $this->getBasicAuth(),
+            'bestAccept' => $this->getBestAccept(),
+            'bestCharset' => $this->getBestCharset(),
+            'bestLanguage' => $this->getBestLanguage(),
+            'clientAddress' => $this->getClientAddress(),
+            'clientCharsets' => $this->getClientCharsets(),
+            'contentType' => $this->getContentType(),
+            'digestAuth' => $this->getDigestAuth(),
+            'httpHost' => $this->getHttpHost(),
+            'uri' => $this->getURI(),
+            'serverName' => $this->getServerName(),
+            'serverAddress' => $this->getServerAddress(),
+            'method' => $this->getMethod(),
+            'port' => $this->getPort(),
+            'httpReferer' => $this->getHTTPReferer(),
+            'languages' => $this->getLanguages(),
+            'scheme' => $this->getScheme(),
+            'isAjax' => $this->isAjax(),
+            'isGet' => $this->isGet(),
+            'isDelete' => $this->isDelete(),
+            'isHead' => $this->isHead(),
+            'isPatch' => $this->isPatch(),
+            'isConnect' => $this->isConnect(),
+            'isTrace' => $this->isTrace(),
+            'isPut' => $this->isPut(),
+            'isPurge' => $this->isPurge(),
+            'isOptions' => $this->isOptions(),
+            'isSoap' => $this->isSoap(),
+            'isSecure' => $this->isSecure(),
+            'isCors' => $this->isCors(),
+            'isPreflight' => $this->isPreflight(),
+            'isSameOrigin' => $this->isSameOrigin(),
+            'isValidHttpMethod' => $this->isValidHttpMethod($this->getMethod()),
+        ];
     }
 }
