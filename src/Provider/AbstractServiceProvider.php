9248e5dddb0b7a001d3ac93864c4d3a300eca567
Julien Turbide <moi@jturbide.com>
2023-03-24 09:48:03 -0400

    service provider refactoring

--- src/Provider/AbstractServiceProvider.php
+++ src/Provider/AbstractServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -16,29 +17,21 @@ use Zemit\Di\Injectable;
 
 /**
  * Class AbstractServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider
  */
 abstract class AbstractServiceProvider extends Injectable implements ServiceProviderInterface
 {
     /**
      * The Service name.
-     * @var string
      */
-    protected $serviceName;
+    protected string $serviceName;
     
+    /**
+     * Set DI and run configure();
+     */
     final public function __construct(DiInterface $di)
     {
         if (!$this->serviceName) {
-            throw new LogicException(
-                sprintf('The service provider defined in "%s" cannot have an empty name.', get_class($this))
-            );
+            throw new LogicException(sprintf('The service provider defined in "%s" cannot have an empty name.', get_class($this)));
         }
         $this->setDI($di);
         $this->configure();
@@ -46,29 +39,23 @@ abstract class AbstractServiceProvider extends Injectable implements ServiceProv
     
     /**
      * {@inheritdoc}
-     *
-     * @return string
      */
-    public function getName()
+    public function getName(): string
     {
         return $this->serviceName;
     }
     
     /**
      * {@inheritdoc}
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
     }
     
     /**
      * {@inheritdoc}
-     *
-     * @return void
      */
-    public function configure()
+    public function configure(): void
     {
     }
 }
