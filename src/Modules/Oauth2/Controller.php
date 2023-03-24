b0f224e6a749c42eef40651c6e2ca86a8daaaa8a
Julien Turbide <moi@jturbide.com>
2023-03-24 12:58:18 -0400

    module oauth2 refactoring

--- src/Modules/Oauth2/Controller.php
+++ src/Modules/Oauth2/Controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -10,17 +11,6 @@
 
 namespace Zemit\Modules\Oauth2;
 
-/**
- * Class Controller
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Modules\Frontend
- */
 class Controller extends \Zemit\Mvc\Controller
 {
 
