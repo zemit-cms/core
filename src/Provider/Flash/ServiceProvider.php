750312ef034315e40d4ff05704b2fc58e21765f2
Julien Turbide <moi@jturbide.com>
2023-03-23 22:50:25 -0400

    flash service provider refactor

--- src/Provider/Flash/ServiceProvider.php
+++ src/Provider/Flash/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -11,65 +12,30 @@
 namespace Zemit\Provider\Flash;
 
 use Phalcon\Flash\Direct;
-use Phalcon\Flash\Session;
 use Zemit\Provider\AbstractServiceProvider;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Flash
- */
 class ServiceProvider extends AbstractServiceProvider
 {
-    /**
-     * The Service name.
-     * @var string
-     */
-    protected $serviceName = 'flash';
+    protected string $serviceName = 'flash';
     
-    protected $cssStyle = [
+    protected array $cssStyle = [
         'error' => 'alert alert-danger fade in',
         'success' => 'alert alert-success fade in',
         'notice' => 'alert alert-info fade in',
         'warning' => 'alert alert-warning fade in',
     ];
     
-    /**
-     * {@inheritdoc}
-     *
-     * Register the Flash Service with the Twitter Bootstrap classes.
-     *
-     * @return void
-     */
     public function register(\Phalcon\Di\DiInterface $di): void
     {
         $cssStyle = $this->cssStyle;
-        
-        $di->setShared($this->getName(), function() use ($di, $cssStyle) {
-            $flash = new Direct();
+        $di->setShared($this->getName(), function () use ($di, $cssStyle) {
             
+            $flash = new Direct();
             $flash->setAutoescape(true);
             $flash->setDI($di);
             $flash->setCssClasses($cssStyle);
             
             return $flash;
         });
-        
-        // @todo flash session in its own service provider
-//        $di->setShared($this->getName() . 'Session', function () use ($di, $cssStyle) {
-//            $flash = new Session();
-//
-//            $flash->setAutoescape(true);
-//            $flash->setDI($di);
-//            $flash->setCssClasses($cssStyle);
-//
-//            return $flash;
-//        });
     }
 }
