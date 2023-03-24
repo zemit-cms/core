7fd0452da7519374c606a672fc160c21b444d1e7
Julien Turbide <moi@jturbide.com>
2023-03-23 23:17:39 -0400

    escaper service provider refactor

--- src/Provider/Escaper/ServiceProvider.php
+++ src/Provider/Escaper/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -14,32 +15,15 @@ use Phalcon\Di\DiInterface;
 use Zemit\Escaper;
 use Zemit\Provider\AbstractServiceProvider;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Escaper
- */
 class ServiceProvider extends AbstractServiceProvider
 {
-    protected $serviceName = 'escaper';
+    protected string $serviceName = 'escaper';
     
-    /**
-     * {@inheritdoc}
-     *
-     * @param DiInterface $di
-     */
-    public function register(\Phalcon\Di\DiInterface $di): void
+    public function register(DiInterface $di): void
     {
-        $di->setShared($this->getName(), function() use ($di) {
-            $escaper = new Escaper();
+        $di->setShared($this->getName(), function () use ($di) {
             
-            return $escaper;
+            return new Escaper();
         });
     }
 }
