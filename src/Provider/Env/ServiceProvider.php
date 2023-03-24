c798eabf9dc665882c232f4a86ebf968aa3ef3f3
Julien Turbide <moi@jturbide.com>
2023-03-23 23:18:44 -0400

    env service provider refactor

--- src/Provider/Env/ServiceProvider.php
+++ src/Provider/Env/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -8,36 +9,16 @@
  * file that was distributed with this source code.
  */
 
-namespace Zemit\Provider\Environment;
+namespace Zemit\Provider\Env;
 
 use Phalcon\Di\DiInterface;
 use Zemit\Provider\AbstractServiceProvider;
 use Zemit\Utils\Env;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Environment
- */
 class ServiceProvider extends AbstractServiceProvider
 {
-    /**
-     * The Service name.
-     * @var string
-     */
-    protected $serviceName = 'env';
+    protected string $serviceName = 'env';
     
-    /**
-     * {@inheritdoc}
-     *
-     * @return void
-     */
     public function register(DiInterface $di): void
     {
         $di->setShared($this->getName(), Env::class);
