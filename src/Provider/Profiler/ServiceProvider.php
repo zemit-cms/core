da28f6589e35b994848400b864f6089a42d51453
Julien Turbide <moi@jturbide.com>
2023-03-27 18:42:35 -0400

    profiler service provider refactoring

--- src/Provider/Profiler/ServiceProvider.php
+++ src/Provider/Profiler/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -14,26 +15,10 @@ use Phalcon\Di\DiInterface;
 use Zemit\Db\Profiler;
 use Zemit\Provider\AbstractServiceProvider;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Profiler
- */
 class ServiceProvider extends AbstractServiceProvider
 {
-    protected $serviceName = 'profiler';
+    protected string $serviceName = 'profiler';
     
-    /**
-     * {@inheritdoc}
-     *
-     * @param DiInterface $di
-     */
     public function register(DiInterface $di): void
     {
         $di->setShared($this->getName(), Profiler::class);
