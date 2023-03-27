293d57cd76fce71d82d23977d545cdc79d339c80
Julien Turbide <moi@jturbide.com>
2023-03-27 18:18:36 -0400

    bootstrap refactoring

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -10,15 +11,12 @@
 
 namespace Zemit;
 
-use Dotenv\Environment\DotenvFactory;
-use Dotenv\Loader;
 use Phalcon\Di;
 use Phalcon\Di\FactoryDefault;
-use Phalcon\Http\Response;
 use Phalcon\Http\ResponseInterface;
 use Phalcon\Text;
 use Phalcon\Events;
-
+use Zemit\Utils\Env;
 use Zemit\Bootstrap\Prepare;
 use Zemit\Bootstrap\Config;
 use Zemit\Bootstrap\Services;
@@ -28,51 +26,49 @@ use Zemit\Events\EventsAwareTrait;
 use Zemit\Mvc\Application;
 use Zemit\Cli\Console;
 use Zemit\Cli\Router as CliRouter;
-
 use Dotenv\Dotenv;
 use Docopt;
-use Zemit\Utils\Env;
 
 /**
  * Class Bootstrap
  * Zemit Core's Bootstrap for the MVC Application & CLI Console mode
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
  *
  * @package Zemit
  */
 class Bootstrap
 {
     use EventsAwareTrait;
-
-    /**
-     * Bootstrap modes
-     */
-    const MODE_CLI = 'console';
-    const MODE_DEFAULT = 'default';
-    const MODE_CONSOLE = self::MODE_CLI;
-
+    
+    
+    public const MODE_CLI = 'cli';
+    
+    
+    public const MODE_MVC = 'mvc';
+    
+    
+    public const MODE_DEFAULT = self::MODE_MVC;
+    
+    
+    public const MODE_CONSOLE = self::MODE_CLI;
+    
+    
     /**
      * Ideally, only the config service provider should be added here, then it will load other service from itself
      * You can also add new Service Providers here if it's absolutely required to be loaded earlier before
      * @var ?array abstract => concrete
      */
-    public ?array $providers = [
+    public array $providers = [
         Provider\Config\ServiceProvider::class => Provider\Config\ServiceProvider::class,
     ];
-
+    
     /**
      * Bootstrap mode
      * @var string
      */
     public string $mode;
-
+    
     /**
-     * Bootstrap console args
+     * Bootstrap cli args
      * - This variable is currently filled by `docopt`
      * @var ?array
      */
@@ -83,7 +79,7 @@ class Bootstrap
      * @var null|string|FactoryDefault|FactoryDefault\Cli
      */
     public $di;
-
+    
     /**
      * @var null|string|Dotenv
      */
@@ -93,52 +89,57 @@ class Bootstrap
      * @var null|string|Docopt
      */
     public $docopt;
-
+    
     /**
      * @var null|string|Prepare
      */
     public $prepare;
-
+    
     /**
      * @var null|string|Config
      */
     public $config;
-
+    
     /**
      * @var null|string|Services
      */
     public $services;
-
+    
     /**
-     * @var null|string|Application|\Phalcon\Cli\Console
+     * @var null|string|Application
      */
     public $application;
-
+    
+    /**
+     * @var null|string|\Phalcon\Cli\Console
+     */
+    public $console;
+    
     /**
      * @var null|string|Modules
      */
     public $modules;
-
+    
     /**
      * @var null|string|Router
      */
     public $router;
-
+    
     /**
      * @var null|string|Debug
      */
     public $debug;
-
+    
     /**
      * @var bool|ResponseInterface
      */
     public $response;
-
+    
     /**
      * @var string
      */
-    public string $consoleDoc = <<<DOC
-Zemit Console
+    public string $cliDoc = <<<DOC
+Zemit CLI
 
 Usage:
   zemit <module> <task> [<action>] [--help | --quiet | --verbose] [--debug] [--format=<format>] [<args>...] [-c <fds>=<value>]
@@ -159,14 +160,14 @@ Options:
 The most commonly used zemit commands are:
    deployment        Populate the database
 DOC;
-
+    
     /**
      * Bootstrap constructor.
      * Setup the di, env, app, config, services, applications, modules and then the router
      *
-     * @param string $mode Mode for the application 'default' 'console'
+     * @param string $mode Mode for the application 'default' 'cli'
      *
-     * @throws Exception
+     * @throws \Exception
      */
     public function __construct(string $mode = self::MODE_DEFAULT)
     {
@@ -182,66 +183,64 @@ DOC;
         $this->debug();
         $this->services();
         $this->application();
+        $this->console();
         $this->modules();
         $this->router();
     }
-
+    
     /**
      * Initialisation
      */
     public function initialize()
     {
     }
-
+    
     /**
      * Prepare the DI including itself (Bootstrap) and setup as default DI
-     * Also use the cli factory default for console mode
+     * Also use the cli factory default for cli mode
      * @return FactoryDefault|FactoryDefault\Cli Return a factory default DI
-     * @throws Exception
+     * @throws \Exception
      */
     public function di()
     {
-        // Use the phalcon cli factory default for console mode
-        $this->fireSet(
-            $this->di,
-            $this->isConsole() ?
+        // Use the phalcon cli factory default for cli mode
+        $this->fireSet($this->di, $this->isCli() ?
             FactoryDefault\Cli::class :
-            FactoryDefault::class,
-            [],
-            function (Bootstrap $bootstrap) {
-                // Register bootstrap itself
+            FactoryDefault::class, [], function (Bootstrap $bootstrap) {
+            
+            // Register bootstrap itself
                 $this->di->setShared('bootstrap', $bootstrap);
-
-                // Set as the default DI
+            
+            // Set as the default DI
                 Di::setDefault($this->di);
-            }
-        );
-
+            });
         return $this->di;
     }
-
+    
     /**
      * Reading .env file
      * @return Dotenv
-     * @throws Exception
+     * @throws \Exception
      */
     public function dotenv()
     {
         return $this->fireSet($this->dotenv, Env::class, [], function (Bootstrap $bootstrap) {
+            
             $bootstrap->dotenv = Env::getDotenv();
         });
     }
-
+    
     /**
-     * Get arguments from command line interface (cli / console)
+     * Get arguments from command line interface cli
      * @return Docopt
-     * @throws Exception
+     * @throws \Exception
      */
     public function docopt()
     {
         return $this->fireSet($this->docopt, Docopt::class, [], function (Bootstrap $bootstrap) {
-            if ($this->isConsole()) {
-                $docoptResponse = $bootstrap->docopt->handle($bootstrap->consoleDoc, [
+            
+            if ($this->isCli()) {
+                $docoptResponse = $bootstrap->docopt->handle($bootstrap->cliDoc, [
                     'argv' => array_slice($_SERVER['argv'], 1),
                     'optionsFirst' => true,
                 ]);
@@ -249,24 +248,23 @@ DOC;
             }
         });
     }
-
+    
     /**
      * Preparing some native PHP related stuff
      * @return Prepare
-     * @throws Exception
+     * @throws \Exception
      */
     public function prepare()
     {
         return $this->fireSet($this->prepare, Prepare::class);
     }
-
+    
     /**
      * Registering bootstrap providers
      */
     public function register(array &$providers = null)
     {
         $providers ??= $this->providers;
-
         foreach ($providers as $key => $provider) {
             if (is_string($provider) && class_exists($provider)) {
                 $provider = new $provider($this->di);
@@ -276,17 +274,17 @@ DOC;
                 }
             }
         }
-
+        
         return $this->providers;
     }
-
+    
     /**
      * Prepare the config service
      * - Fire events (before & after)
-     * - Apply current bootstrap mode ('default', 'console')
+     * - Apply current bootstrap mode ('default', 'cli')
      * - Merge with current environment config
      * @return Config
-     * @throws Exception
+     * @throws \Exception
      */
     public function config()
     {
@@ -295,29 +293,32 @@ DOC;
         }
         return $this->fireSet($this->config, Config::class);
     }
-
+    
     /**
      * @return Debug
-     * @throws Exception
+     * @throws \Exception
      */
     public function debug()
     {
         return $this->fireSet($this->debug, Debug::class, [], function (Bootstrap $bootstrap) {
+            
             $config = $bootstrap->config->debug->toArray();
-            $bootstrap->prepare->debug($bootstrap->config);
-
+            $debug = $bootstrap->config->app->get('debug') || $config['enable'];
+            $bootstrap->prepare->debug($debug);
+            
             // @todo review on phalcon5 php8+ because phalcon4 php8+ debug is doing cyclic error
             $cyclicError =
                 version_compare(PHP_VERSION, '8.0.0', '>=') &&
                 version_compare(\Phalcon\Version::get(), '5.0.0', '<');
-
-            if (!$this->isConsole() && !$cyclicError) {
-                if ($bootstrap->config->app->get('debug') || $config['enable']) {
+            
+            if (!$this->isCli() && !$cyclicError) {
+                if ($debug) {
                     $bootstrap->debug->listen($config['exception'] ?? true, $config['lowSeverity'] ?? false);
                     $bootstrap->debug->setBlacklist($config['blacklist'] ?? []);
                     $bootstrap->debug->setShowFiles($config['showFiles'] ?? true);
                     $bootstrap->debug->setShowBackTrace($config['showBackTrace'] ?? true);
                     $bootstrap->debug->setShowFileFragment($config['showFileFragment'] ?? true);
+                    
                     if (is_string($config['uri'])) {
                         $bootstrap->debug->setUri($config['uri']);
                     }
@@ -325,58 +326,69 @@ DOC;
             }
         });
     }
-
+    
     /**
      * Prepare procedural services way
      * - Fire events (before & after)
      * - Pass the current Di object as well as the current Config object
      * @return Services
-     * @throws Exception
+     * @throws \Exception
      */
     public function services()
     {
-        return $this->fireSet($this->services, Services::class, [$this->di, $this->config]);
+        return $this->fireSet($this->services, Services::class, [$this->di]);
     }
-
+    
     /**
      * Prepare the application
      * - Fire events (before & after)
      * - Pass the current Di object
-     * - Depends on the current bootstrap mode ('default', 'console')
-     * @return Console|Application
-     * @throws Exception
+     * @return Application
+     * @throws \Exception
      */
     public function application()
     {
-        return $this->fireSet($this->application, $this->isConsole() ? Console::class : Application::class, [$this->di]);
+        return $this->fireSet($this->application, Application::class, [$this->di]);
     }
-
+    
+    /**
+     * @return mixed|null
+     * @throws \Exception
+     */
+    public function console()
+    {
+        return $this->fireSet($this->console, Console::class, [$this->di]);
+    }
+    
     /**
      * Prepare the application for modules
      * - Fire events (before & after)
      * - Pass the current Application object
      * @return Modules
-     * @throws Exception
+     * @throws \Exception
      */
     public function modules()
     {
-        return $this->fireSet($this->modules, Modules::class, [$this->application]);
+        $modules = $this->config->modules->toArray();
+        $defaultModule = $this->config->path('router.defaults.module');
+        
+        return $this->fireSet($this->modules, Modules::class, [$this->isMvc() ? $this->application : $this->console, $modules, $defaultModule]);
     }
-
+    
     /**
      * Prepare the router
      * @return Router
-     * @throws Exception
+     * @throws \Exception
      */
     public function router()
     {
         if ($this->di->has('router')) {
             $this->router = $this->di->get('router');
         }
-    
-        return $this->isConsole() ?
+        
+        return $this->isCli() ?
             $this->fireSet($this->router, CliRouter::class, [true]) :
-            $this->fireSet($this->router, Router::class, [true, $this->application]);
+            $this->fireSet($this->router, Router::class, [true, $this->config]);
     }
     
     /**
@@ -390,27 +402,16 @@ DOC;
     public function run()
     {
         $this->fire('beforeRun');
-        
-        if (isset($this->application)) {
+        if ($this->isMvc() && isset($this->application)) {
             
-            if ($this->application instanceof Console) {
-                
-                // run console cli
-                $content = $this->handleConsole($this->application);
-            }
+            $content = $this->handleApplication($this->application);
+        }
+        elseif ($this->isCli() && isset($this->console)) {
             
-            else if ($this->application instanceof Application) {
-                
-                // run mvc http
-                $content = $this->handleApplication($this->application);
-            }
-            else {
-                $className = is_object($this->application)? get_class($this->application) : $this->application;
-                throw new \Exception('Application \'' . $className . '\' not supported', 400);
-            }
+            $content = $this->handleConsole($this->console);
         }
         else {
-            throw new \Exception('Application not found', 404);
+            throw new \Exception('Application or Console not found', 404);
         }
         
         $this->fire('afterRun', $content);
@@ -424,7 +425,6 @@ DOC;
     public function handleConsole(Console $console)
     {
         $response = null;
-        
         try {
             ob_start();
             $console->handle($this->getArguments());
@@ -453,12 +453,12 @@ DOC;
         $this->response = $application->handle($_SERVER['REQUEST_URI'] ?? '/');
         return $this->response ? $this->response->getContent() : '';
     }
-
+    
     /**
      * Get & format arguments from the $this->args property
      * @return array Key value pair, human-readable
      */
-    public function getArguments() : array
+    public function getArguments(): array
     {
         $arguments = [];
         if (!empty($this->args)) {
@@ -471,21 +471,45 @@ DOC;
         }
         return $arguments;
     }
-
+    
     /**
-     * Return true if the bootstrap mode is set to 'console'
-     * @return bool Console mode
+     * Return true if the bootstrap mode is set to 'cli'
      */
-    public function isConsole() : bool
+    public function isCli(): bool
     {
-        return $this->getMode() === self::MODE_CONSOLE;
+        return $this->getMode() === self::MODE_CLI;
     }
-
+    
     /**
-     * Return the raw bootstrap mode, should be either 'console' or 'default'
-     * @return string Bootstrap mode
+     * Return true if the bootstrap mode is set to 'mvc'
      */
-    public function getMode() : string
+    public function isMvc(): bool
+    {
+        return $this->getMode() === self::MODE_MVC;
+    }
+    
+    /**
+     * Alias for the ->isCli() method
+     * @deprecated
+     */
+    public function isConsole(): bool
+    {
+        return $this->isCli();
+    }
+    
+    /**
+     * Alias for the ->isMvc() method
+     * @deprecated
+     */
+    public function isDefault(): bool
+    {
+        return $this->isMvc();
+    }
+    
+    /**
+     * Return the current mode
+     */
+    public function getMode(): string
     {
         return $this->mode;
     }
