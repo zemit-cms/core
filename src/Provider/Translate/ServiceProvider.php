17c2de4054c3607c33f463b583787807058f5a08
Julien Turbide <moi@jturbide.com>
2023-03-27 18:29:09 -0400

    translate service provider refactoring

--- src/Provider/Translate/ServiceProvider.php
+++ src/Provider/Translate/ServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -13,49 +14,22 @@ namespace Zemit\Provider\Translate;
 use Phalcon\Di\DiInterface;
 use Phalcon\Translate\Adapter\Gettext;
 use Phalcon\Translate\InterpolatorFactory;
+use Zemit\Config\ConfigInterface;
 use Zemit\Provider\AbstractServiceProvider;
 
-/**
- * Class ServiceProvider
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Provider\Translate
- */
 class ServiceProvider extends AbstractServiceProvider
 {
+    protected string $serviceName = 'translate';
     
-    // @todo
-    const DEFAULT_OPTIONS = [
-    
-    ];
-    
-    /**
-     * The Service name.
-     * @var string
-     */
-    protected $serviceName = 'translate';
-    
-    /**
-     * {@inheritdoc}
-     *
-     * Register the Flash Service with the Twitter Bootstrap classes.
-     *
-     * @return void
-     */
     public function register(DiInterface $di): void
     {
-        $di->setShared($this->getName(), function() use ($di) {
-            $config = $di->get('config');
-            if ($config && $config->has('translate')) {
-                $options = $config->translate->toArray();
-            }
+        $di->setShared($this->getName(), function () use ($di) {
             
-            $translate = new Gettext(new InterpolatorFactory(), $options ?? self::DEFAULT_OPTIONS);
+            $config = $di->get('config');
+            assert($config instanceof ConfigInterface);
+            $translateConfig = $config->pathToArray('translate') ?? [];
+            
+            $translate = new Gettext(new InterpolatorFactory(), $translateConfig);
             $translate->setLocale(LC_MESSAGES, $di->get('locale')->get() . '.utf8');
             
             return $translate;
