d5a0b1375f8abf7df23d6e35d00dda9f5c88de23
Julien Turbide <moi@jturbide.com>
2023-03-28 01:46:44 -0400

    php entry point refactoring

--- public/index.php
+++ public/index.php
@@ -10,13 +10,11 @@
 
 use Zemit\Bootstrap;
 
-// Get the fallback root, vendor, app path and default app namespace
-defined('VENDOR_PATH') || define('VENDOR_PATH', (getenv('VENDOR_PATH') ? getenv('VENDOR_PATH') : dirname(__DIR__) . '/vendor'));
-defined('APP_NAMESPACE') || define('APP_NAMESPACE', (getenv('APP_NAMESPACE') ? getenv('APP_NAMESPACE') : 'Zemit'));
-defined('APP_PATH') || define('APP_PATH', (getenv('APP_PATH') ? getenv('APP_PATH') : dirname(__DIR__) . '/src'));
+// Set your default path and namespace
+const VENDOR_PATH = __DIR__ . '/../vendor';
+const APP_PATH = __DIR__ . '/../src';
+const APP_NAMESPACE = 'Zemit';
 
-// Register Composer Autoloader
+// Register autoloader and run bootstrap
 $composer = require_once VENDOR_PATH . '/autoload.php';
-
-// Run Zemit CMS
-echo (new Bootstrap())->run();
+echo (new Bootstrap(Bootstrap::MODE_MVC))->run();
