54181c5d1a10d51b93607bc1b0c585cd8d168277
Julien Turbide <moi@jturbide.com>
2023-04-02 14:38:45 -0400

    config service provider refactoring

--- src/Provider/Config/ServiceProvider.php
+++ src/Provider/Config/ServiceProvider.php
@@ -14,37 +14,34 @@ namespace Zemit\Provider\Config;
 use Phalcon\Di\DiInterface;
 use Zemit\Bootstrap;
 use Zemit\Bootstrap\Config;
+use Zemit\Config\ConfigInterface;
 use Zemit\Provider\AbstractServiceProvider;
+use Zemit\Support\Php;
 
 class ServiceProvider extends AbstractServiceProvider
 {
     protected string $serviceName = 'config';
     
+    protected ConfigInterface $config;
+    
     public function register(DiInterface $di = null): void
     {
         // Set shared service in DI
         $di->setShared($this->getName(), function () use ($di) {
-            
+    
             $bootstrap = $di->get('bootstrap');
             assert($bootstrap instanceof Bootstrap);
             
-            $config = $bootstrap->config ?? new Config();
-            
-            if (is_string($config) && class_exists($config)) {
-                $config = new $config();
-            }
-            
-            // Set bootstrap mode into config
-            $config->mode = $di->get('bootstrap')->getMode();
+            $bootstrap->config ??= new Config();
             
             // Merge config with current environment
-            $config->mergeEnvConfig();
+            $bootstrap->config->mergeEnvConfig();
             
             // Launch bootstrap prepare raw php configs
-            $bootstrap->prepare->php($config->pathToArray('app') ?? []);
+            Php::set($bootstrap->config->pathToArray('app') ?? []);
             
             // Set the config
-            return $config;
+            return $bootstrap->config;
         });
     }
 }
