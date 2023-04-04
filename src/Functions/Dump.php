70a13a9b1ec4ec48cfcb092637d33862af94cecc
Julien Turbide <moi@jturbide.com>
2023-04-04 13:03:35 -0400

    adding vdd function and exit_500 exit 500 on dd and vdd if not using cli mode

--- src/Functions/Dump.php
+++ src/Functions/Dump.php
@@ -2,6 +2,17 @@
 
 use Phalcon\Debug\Dump;
 
+if (!function_exists('vdd')) {
+    /**
+     * Dump the passed variables and end the script.
+     */
+    function vdd(...$params): void
+    {
+        var_dump(...$params);
+        exit_500();
+    }
+}
+
 if (!function_exists('dd')) {
     /**
      * Dump the passed variables and end the script.
@@ -9,7 +20,7 @@ if (!function_exists('dd')) {
     function dd(...$params): void
     {
         dump(...$params);
-        exit(1);
+        exit_500();
     }
 }
 
@@ -26,5 +37,18 @@ if (!function_exists('dump')) {
             }
             echo $ret;
         }
+    }
+}
+
+if (!function_exists('exit_500')) {
+    /**
+     * Exit and set header
+     */
+    function exit_500(): void
+    {
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+            header('500 Internal Server Error');
+        }
+        exit(1);
     }
 }
