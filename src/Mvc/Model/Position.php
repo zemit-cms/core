7e679521c30c2d2b26f19e5edac2338762edbf4c
Julien Turbide <moi@jturbide.com>
2023-04-05 21:12:00 -0400

    refactoring model position trait

--- src/Mvc/Model/Position.php
+++ src/Mvc/Model/Position.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -10,106 +11,65 @@
 
 namespace Zemit\Mvc\Model;
 
-use Phalcon\Text;
+use Exception;
 use Zemit\Mvc\Model;
+use Zemit\Mvc\Model\Behavior\Position as PositionBehavior;
 
-/**
- * Trait Position
- * @todo add a setting to do either raw sql query or use the complete orm with events
- * @todo fix it to make it work again
- * @todo refactor to use behavior instead
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Mvc\Model
- */
 trait Position
 {
+    use Model\AbstractTrait\AbstractBehavior;
+    use Attribute;
+    use Options;
     
-    protected $_positionSettings;
+    public Model\Behavior\Position $positionBehavior;
     
-    protected function _setPosition($field = 'position', $idField = 'id')
+    /**
+     * Initializing Position
+     */
+    public function initializePosition(?array $options = null): void
     {
+        $options ??= $this->getOptionsManager()->get('position') ?? [];
+        $this->setPositionBehavior(new PositionBehavior($options));
+    }
     
-        $this->_positionSettings['field'] = $field;
-        $this->_positionSettings['idField'] = $idField;
-        
-        if (property_exists($this, $field) && property_exists($this, $idField)) {
-            $this->getEventsManager()->attach('model', function ($event, Model $entity) use ($field, $idField) {
-                switch ($event->getType()) {
-                    case 'beforeValidation':
-                        // if position field is empty, force current max(position)+1
-                        $lastPosition = self::findFirst(['order' => $field . ' DESC']);
-                        $this->$field = $lastPosition ? $lastPosition->$field + 1 : 1;
-                        break;
-                    case 'afterSave':
-                        if ($entity->hasSnapshotData() && $entity->hasUpdated($field)) {
-                            $snapshot = $entity->getOldSnapshotData() ?: $entity->getSnapshotData();
-                            $entityPosition = $entity->{'get' . ucfirst($field)}();
-                            $entityId = $entity->{'get' . ucfirst($idField)}();
-                            
-                            if ($entityPosition instanceof \Phalcon\Db\RawValue) {
-                            } else {
-                                $uField = Text::uncamelize($field);
-//                                ini_set('xdebug.max_nesting_level', 0);
-                                if ($snapshot[$field] > $entityPosition) {
-//                                    $updatePositionQuery = 'UPDATE [' . get_class($entity) . '] SET ['.$field.'] = ['.$field.']+1 WHERE ['.$field.'] >= ?1 and ['.$field.'] < ?2 and ['.$idField.'] <> ?0';
-                                    $updatePositionRaw = 'UPDATE `'.$entity->getSource().'` SET `'.$uField.'` = `'.$uField.'`+1 WHERE `'.$uField.'` >= :position and `'.$uField.'` < :oldPosition and `'.$idField.'` <> :id';
-                                } elseif ($snapshot[$field] < $entityPosition) {
-//                                    $updatePositionQuery = 'UPDATE [' . get_class($entity) . '] SET ['.$field.'] = ['.$field.']-1 WHERE ['.$field.'] > ?2 and ['.$field.'] <= ?1 and ['.$idField.'] <> ?0';
-                                    $updatePositionRaw = 'UPDATE `' . $entity->getSource() . '` SET `'.$uField.'` = `'.$uField.'`-1 WHERE `'.$uField.'` > :oldPosition and `'.$uField.'` <= :position and `'.$idField.'` <> :id';
-                                }
-//                                $entity->getModelsManager()->executeQuery($updatePositionQuery, [$entityId, $entityPosition, $snapshot[$field]]);
-                                if (!empty($updatePositionRaw)) {
-                                    $entity->getWriteConnection()->query($updatePositionRaw, [
-                                        'id' => $entityId,
-                                        'position' => $entityPosition,
-                                        'oldPosition' => $snapshot[$field],
-                                    ]);
-                                }
-                            }
-                        }
-                        break;
-                }
-                return true;
-            });
-        }
+    /**
+     * Set Position Behavior
+     */
+    public function setPositionBehavior(PositionBehavior $positionBehavior): void
+    {
+        $this->positionBehavior = $positionBehavior;
+        $this->addBehavior($this->positionBehavior);
+    }
+    
+    /**
+     * Get Position Behavior
+     */
+    public function getPositionBehavior(): PositionBehavior
+    {
+        return $this->positionBehavior;
     }
     
     /**
      * Re-ordering an entity
      * - Update position+1 done using afterSave event
      *
-     * @param int|null $position
-     * @param string|null $field
-     *
      * @return mixed
+     * @throws Exception
      */
-    public function reorder(int $position = null, string $field = null)
+    public function reorder(?int $position = null, ?string $positionField = null)
     {
-        $field ??= $this->_positionSettings['field'] ?? 'position';
-    
-        /**
-         * Call the beforeReorder
-         */
+        $positionField ??= $this->getPositionBehavior()->getField();
+        
         if ($this->fireEventCancel('beforeReorder') === false) {
             return false;
         }
         
-        $this->assign([$field => $position], [$field]);
-        $saved = $this->save() && (!$this->hasSnapshotData() || $this->hasUpdated($field));
-    
-        /**
-         * Call the afterReorder
-         */
+        $this->assign([$positionField => $position], [$positionField]);
+        $saved = $this->save() && (!$this->hasSnapshotData() || $this->hasUpdated($positionField));
+        
         if ($saved) {
             $this->fireEvent('afterReorder');
         }
-        
         
         return $saved;
     }
