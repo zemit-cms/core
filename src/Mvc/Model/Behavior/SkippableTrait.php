4dbf5dca6dd35e9ff9ce74ce361c53732d79eac1
Julien Turbide <moi@jturbide.com>
2023-04-07 11:58:56 -0400

    new progress trait and skippable trait added static progress (global) added static skippable (global)

--- src/Mvc/Model/Behavior/SkippableTrait.php
+++ src/Mvc/Model/Behavior/SkippableTrait.php
@@ -11,15 +11,15 @@
 
 namespace Zemit\Mvc\Model\Behavior;
 
-use Phalcon\Mvc\Model\Behavior;
-use Phalcon\Mvc\ModelInterface;
-
 /**
  * Allow to enable or disable trait
+ * on the current model instance ($enabled)
+ * or globally for every model instance ($staticEnabled)
  */
 trait SkippableTrait
 {
     public bool $enabled = true;
+    public static bool $staticEnabled = true;
     
     /**
      * Return true if the behavior is enabled
@@ -37,6 +37,24 @@ trait SkippableTrait
     public function setEnabled(bool $enabled): void
     {
         $this->enabled = $enabled;
+    }
+    
+    /**
+     * Return true if the behavior is enabled
+     * globally for every model instance
+     */
+    public static function getStaticEnabled(): bool
+    {
+        return self::$staticEnabled;
+    }
+    
+    /**
+     * Set true to enable the behavior
+     * globally for every model instance
+     */
+    public static function setStaticEnabled(bool $staticEnabled): void
+    {
+        self::$staticEnabled = $staticEnabled;
     }
     
     /**
@@ -58,11 +76,38 @@ trait SkippableTrait
     }
     
     /**
+     * Enable the behavior
+     * globally for every model instance
+     */
+    public static function staticEnable(): void
+    {
+        self::setStaticEnabled(true);
+    }
+    
+    /**
+     * Disable the behavior
+     * globally for every model instance
+     */
+    public static function staticDisable(): void
+    {
+        self::setStaticEnabled(false);
+    }
+    
+    /**
      * Return true if the behavior is enabled
-     * on the current model instance
+     * on the current model instance and globally
      */
     public function isEnabled(): bool
     {
-        return $this->getEnabled();
+        return $this->getEnabled() && self::getStaticEnabled();
+    }
+    
+    /**
+     * Return true if the behavior is enabled
+     * on the current model instance and globally
+     */
+    public function isDisabled(): bool
+    {
+        return !$this->isEnabled();
     }
 }
