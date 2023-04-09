71d329a8bb20d470aab66fcbf8b8205f4f7d5aea
Julien Turbide <moi@jturbide.com>
2023-04-09 11:59:03 -0400

    refactoring

--- src/Mvc/View/Error.php
+++ src/Mvc/View/Error.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Zemit Framework.
  *
@@ -10,40 +11,17 @@
 
 namespace Zemit\Mvc\View;
 
-use Zemit\Di\Injectable;
+use Phalcon\Di\Injectable;
 use Phalcon\Events\Event;
 use Phalcon\Mvc\ViewInterface;
 
-/**
- * Class Error
- *
- * @author Julien Turbide <jturbide@nuagerie.com>
- * @copyright Zemit Team <contact@zemit.com>
- *
- * @since 1.0
- * @version 1.0
- *
- * @package Zemit\Mvc\View
- */
 class Error extends Injectable
 {
-    /**
-     * @param Event $event
-     * @param ViewInterface $view
-     * @param null $currentView
-     */
     public function beforeRenderView(Event $event, ViewInterface $view, $currentView = null)
     {
-    
     }
     
-    /**
-     * @param Event $event
-     * @param ViewInterface $view
-     * @param null $currentView
-     */
     public function notFoundView(Event $event, ViewInterface $view, $currentView = null)
     {
-    
     }
 }
