271027d8f9ecf9b4f929f3fdf6d1f8253497ef9f
Julien Turbide <moi@jturbide.com>
2023-04-14 10:00:53 -0400

    new index loader

--- public/index.php
+++ public/index.php
@@ -8,15 +8,4 @@
  * file that was distributed with this source code.
  */
 
-use Phalcon\Loader;
-
-// Set your default path and namespace
-const VENDOR_PATH = __DIR__ . '/../vendor/';
-const APP_PATH = __DIR__ . '/../src/';
-
-$loader = new Loader();
-$loader->registerFiles([VENDOR_PATH . 'autoload.php']);
-$loader->registerNamespaces(['Zemit' => APP_PATH]);
-$loader->register();
-
-echo (new Zemit\Bootstrap(Zemit\Bootstrap::MODE_MVC))->run();
+require '../index.php';
