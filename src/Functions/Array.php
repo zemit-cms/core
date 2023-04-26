c28bb23c8ffb433665762cd398fbff00907c8924
Julien Turbide <moi@jturbide.com>
2023-04-26 12:26:57 -0400

    new array_search_key_recursive function

--- src/Functions/Array.php
+++ src/Functions/Array.php
@@ -11,9 +11,26 @@ if (!function_exists('array_unset_recursive')) {
                 $removeCount++;
             }
             elseif (is_array($element)) {
-                array_unset_recursive($array[$key], $keyList);
+                array_unset_recursive($array[$key], $keyList, $strict);
             }
         }
         return $removeCount;
     }
 }
+
+if (!function_exists('array_search_key_recursive')) {
+    
+    function array_search_key_recursive(array &$array, array $keyList, bool $strict = true): int
+    {
+        foreach ($array as $key => $element) {
+            if (in_array($key, $keyList, $strict)) {
+                return true;
+            }
+            elseif (is_array($element)) {
+                return array_search_key_recursive($array[$key], $keyList, $strict);
+            }
+        }
+        return false;
+    }
+}
+
