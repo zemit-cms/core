9e6937a5d1d55a5099b3ba16def9a26cde2519e1
Julien Turbide <moi@jturbide.com>
2023-07-13 16:34:43 -0400

    fix regenerate all model script

--- bin/regenerate-all-models.sh
+++ bin/regenerate-all-models.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf ./src/Models/Abstracts/Abstract*.php && ./vendor/bin/phalcon all-models --config=./devtools.php --annotate --get-set --camelize --mapcolumn --abstract --doc --directory=./ --output=./src/Models/Abstracts --relations --fk --force --namespace=Zemit\\Models\\Abstracts --extends=\\Zemit\\Models\\AbstractModel "$@" && find ./src/Models/Abstracts/ -type f -exec sed -i -e '/$this->setSchema/i \\t\tparent::initialize();' {} \; && find ./src/Models/Abstracts/ -type f -exec sed -i -e 's/ $this->setSchema/ \/\/ $this->setSchema/g' {} \;
+rm -rf ./src/Models/Abstracts/Abstract*.php && ./vendor/bin/phalcon all-models --config=./devtools.php --get-set --camelize --mapcolumn --abstract --doc --directory=./ --output=./src/Models/Abstracts --relations --fk --force --namespace=Zemit\\Models\\Abstracts --extends=\\Zemit\\Models\\AbstractModel "$@"
+find ./src/Models/Abstracts/ -type f -exec sed -i -e '/$this->setSchema/i \        parent::initialize();' {} \;
+find ./src/Models/Abstracts/ -type f -exec sed -i -e 's/ $this->setSchema/ \/\/ $this->setSchema/g' {} \;
+find ./src/Models/Abstracts/ -type f -exec sed -i -e 's/public function initialize()/public function initialize() :void/g' {} \;
