de55f67a2fadf64e5dbb5c5f803a1a6594444295
Julien Turbide <moi@jturbide.com>
2023-08-09 09:54:40 -0400

    fix code sniffing

--- src/Mvc/Model/Blameable/BlameAt.php
+++ src/Mvc/Model/Blameable/BlameAt.php
@@ -13,10 +13,10 @@ namespace Zemit\Mvc\Model\Blameable;
 
 trait BlameAt
 {
-    public function getDateCallback(string $format, ...$args): \Closure
+    public function getDateCallback(string $format, ?int $timestamp): \Closure
     {
-        return function () use ($format, $args) {
-            return date($format, ...$args);
+        return function () use ($format, $timestamp) {
+            return isset($timestamp)? date($format, $timestamp) : date($format);
         };
     }
 }
