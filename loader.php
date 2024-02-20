52024ec259c74d72c23e55850ef133c7698ddae6
Julien Turbide <moi@jturbide.com>
2024-02-20 12:37:05 -0500

    fixed loader and cli

--- loader.php
+++ loader.php
@@ -9,16 +9,17 @@
  * file that was distributed with this source code.
  */
 
-use Phalcon\Loader;
+use Phalcon\Autoload\Loader;
 
-const APP_NAMESPACE = 'Zemit';
+const APP_NAMESPACE = 'App';
 const ROOT_PATH = __DIR__;
 const VENDOR_PATH = ROOT_PATH . '/vendor';
-const APP_PATH = ROOT_PATH . '/src';
+const APP_PATH = ROOT_PATH . '/app';
 
 $loader = new Loader();
-$loader->registerFiles([VENDOR_PATH . '/autoload.php']);
-$loader->registerNamespaces([APP_NAMESPACE => APP_PATH]);
+$loader->setFiles([VENDOR_PATH . '/autoload.php']);
+$loader->setNamespaces([APP_NAMESPACE => APP_PATH]);
+$loader->setFileCheckingCallback(null);
 $loader->register();
 
 return $loader;
