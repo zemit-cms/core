4a7187e2504d438bc75545067a273b0ead280fb0
Julien Turbide <moi@jturbide.com>
2024-02-20 12:45:26 -0500

    temporary workaround phalcon models meta data mysql binary issue https://github.com/phalcon/cphalcon/issues/16532

---
+++ src/Db/Adapter/Pdo/Mysql.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Db\Adapter\Pdo;
+
+use Phalcon\Db\Column;
+
+class Mysql extends \Phalcon\Db\Adapter\Pdo\Mysql
+{
+    public function describeColumns(string $table, string $schema = null): array
+    {
+        $definitions = parent::describeColumns($table, $schema);
+        
+        if (Column::TYPE_TINYINTEGER !== Column::TYPE_BINARY) {
+            return $definitions;
+        }
+        
+        foreach ($definitions as $definitionKey => $definition) {
+            
+            if ($definition->getType() === Column::TYPE_TINYINTEGER && !$definition->isNumeric()) {
+                // probably a binary at this point
+                
+                $newDefinition = [];
+                
+                // protected to public
+                $prefix = chr(0).'*'.chr(0);
+                foreach ((array)$definition as $key => $value) {
+                    $newDefinition[str_replace($prefix, '', $key)] = $value;
+                }
+                
+                $newDefinition['bindType'] = Column::BIND_PARAM_BLOB;
+                $newDefinition['type'] = Column::TYPE_VARBINARY;
+                unset($newDefinition['scale']);
+                
+                // reset definition
+                $definitions[$definitionKey] = new Column($definition->getName(), $newDefinition);
+            }
+        }
+        
+        return $definitions;
+    }
+}
