7cb1a5ea4a7e45b69e82fee84e9cc13d44fd6794
Julien Turbide <moi@jturbide.com>
2024-02-20 12:36:27 -0500

    added json_validate support

---
+++ src/Functions/Json.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!function_exists('json_validate')) {
+    /**
+     * Validates a JSON string.
+     *
+     * @param string $json The JSON string to validate.
+     * @param int $depth Maximum depth. Must be greater than zero.
+     * @param int $flags Bitmask of JSON decode options.
+     * @return bool Returns true if the string is a valid JSON, otherwise false.
+     */
+    function json_validate($json, $depth = 512, $flags = 0) {
+        if (!is_string($json)) {
+            return false;
+        }
+        
+        try {
+            json_decode($json, false, $depth, $flags | JSON_THROW_ON_ERROR);
+            return true;
+        } catch (\JsonException $e) {
+            return false;
+        }
+    }
+}
