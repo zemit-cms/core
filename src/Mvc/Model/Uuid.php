b9f5d73a88b30f9a3571ca89e7ebb42b3493eabe
Julien Turbide <moi@jturbide.com>
2024-02-20 12:56:11 -0500

    updating the model uuid bahavior to work with native & binary mysql uuid features

--- src/Mvc/Model/Uuid.php
+++ src/Mvc/Model/Uuid.php
@@ -11,6 +11,7 @@
 
 namespace Zemit\Mvc\Model;
 
+use Phalcon\Db\RawValue;
 use Phalcon\Encryption\Security;
 use Zemit\Mvc\Model\AbstractTrait\AbstractBehavior;
 use Zemit\Mvc\Model\AbstractTrait\AbstractInjectable;
@@ -31,17 +32,55 @@ trait Uuid
         $options ??= $this->getOptionsManager()->get('uuid') ?? [];
         
         $field = $options['field'] ?? 'uuid';
+        $native = $options['native'] ?? true;
+        $binary = $options['binary'] ?? true;
         
         $security = $this->getDI()->get('security');
         assert($security instanceof Security);
         
         $this->setUuidBehavior(new Transformable([
             'beforeValidationOnCreate' => [
-                $field => function ($model, $field) use ($security) {
-                    return $model->getAttribute($field) ?? $security->getRandom()->uuid();
+                $field => function ($model, $field) use ($security, $native, $binary) {
+                    return $model->getAttribute($field) ?? $native
+                        ? ($binary
+                            ? new RawValue('UUID_TO_BIN(UUID())')
+                            : new RawValue('UUID()')
+                        )
+                        : ($binary
+                            ? $this->getBinaryUuid($security->getRandom()->uuid())
+                            : $security->getRandom()->uuid()
+                        );
                 },
             ],
+            'afterFetch' => [
+                $field => function ($model, $field) use ($native, $binary) {
+                    $value = $model->getAttribute($field);
+                    if ($binary && !empty($value)) {
+                        $hex = bin2hex($value);
+                        $uuid = sprintf('%s-%s-%s-%s-%s',
+                            substr($hex, 0, 8),
+                            substr($hex, 8, 4),
+                            substr($hex, 12, 4),
+                            substr($hex, 16, 4),
+                            substr($hex, 20, 12)
+                        );
+                        $this->setAttribute($field, $uuid);
+                        return $uuid;
+                    }
+                }
+            ],
         ]));
+    }
+    
+    /**
+     * Get a binary representation of UUID
+     *
+     * @param string $uuid
+     * @return string
+     */
+    private function getBinaryUuid($uuid)
+    {
+        return pack('h*', str_replace('-', '', $uuid));
     }
     
     /**
