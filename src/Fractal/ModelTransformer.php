f98a165908f4e6a40ea24b67a061671edac35704
Julien Turbide <moi@jturbide.com>
2024-02-21 02:15:25 -0500

    bug fix

--- src/Fractal/ModelTransformer.php
+++ src/Fractal/ModelTransformer.php
@@ -4,14 +4,14 @@ namespace Zemit\Fractal;
 
 use League\Fractal\TransformerAbstract;
 use Phalcon\Di\InjectionAwareInterface;
-use Phalcon\Mvc\ModelInterface;
 use Zemit\Di\InjectableTrait;
+use Phalcon\Mvc\Model;
 
 class ModelTransformer extends TransformerAbstract implements InjectionAwareInterface
 {
     use InjectableTrait;
     
-    public function transform(ModelInterface $model): array
+    public function transform(Model $model): array
     {
         return $model->toArray();
     }
