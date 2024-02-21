d37684035b3e7dfc657b64c2ab4bf45fdff5f2e0
Julien Turbide <moi@jturbide.com>
2024-02-21 02:31:47 -0500

    added code sniffer auto fixer to migrations & model generation

--- bin/migration-generate.sh
+++ bin/migration-generate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ./vendor/bin/phalcon migration generate --config=./devtools.php --directory=./ --migrations=./src/Migrations/ --no-auto-increment --force --verbose --log-in-db "$@"
+./vendor/bin/phpcbf --standard=phpcs-eol.xml ./src/Migrations/
