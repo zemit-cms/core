84cf0146adeaab5df6665d51bf48e68bc4324ed9
Julien Turbide <moi@jturbide.com>
2024-02-20 20:01:47 -0500

    fix unit test

--- tests/Unit/AbstractUnit.php
+++ tests/Unit/AbstractUnit.php
@@ -54,17 +54,17 @@ abstract class AbstractUnit extends TestCase
      */
     protected function setUp(): void
     {
-        // Set your default path and namespace
         defined('VENDOR_PATH') || define('VENDOR_PATH', (getenv('VENDOR_PATH') ? getenv('VENDOR_PATH') : dirname(__DIR__) . '/../vendor'));
-        defined('APP_NAMESPACE') || define('APP_NAMESPACE', (getenv('APP_NAMESPACE') ? getenv('APP_NAMESPACE') : 'App'));
-        defined('APP_PATH') || define('APP_PATH', (getenv('APP_PATH') ? getenv('APP_PATH') : dirname(__DIR__) . '/../app'));
+        defined('APP_NAMESPACE') || define('APP_NAMESPACE', (getenv('APP_NAMESPACE') ? getenv('APP_NAMESPACE') : 'Zemit'));
+        defined('APP_PATH') || define('APP_PATH', (getenv('APP_PATH') ? getenv('APP_PATH') : dirname(__DIR__) . '/../src'));
         
         Env::setNames(['.env.testing']);
         
-        $this->loader = new Loader();
-        $this->loader->setFiles([VENDOR_PATH . 'autoload.php']);
-        $this->loader->setNamespaces(['Zemit' => APP_PATH]);
-        $this->loader->register();
+        $loader = new Loader();
+        $loader->setFiles([VENDOR_PATH . '/autoload.php']);
+        $loader->setNamespaces([APP_NAMESPACE => APP_PATH]);
+        $loader->setFileCheckingCallback(null);
+        $loader->register();
         
         $this->bootstrap = new Bootstrap($this->mode);
         $this->di = $this->bootstrap->di;
