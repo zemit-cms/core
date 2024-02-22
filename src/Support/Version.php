c2121e60117afd7f5693fc46f0064096a8629b98
Julien Turbide <moi@jturbide.com>
2024-02-21 20:18:57 -0500

    updating version to fetch composer.json version automatically

--- src/Support/Version.php
+++ src/Support/Version.php
@@ -28,6 +28,8 @@ class Version extends \Phalcon\Support\Version
      */
     protected function getVersion(): array
     {
-        return [5, 4, 0, 1, 0];
+        $composerPath = dirname(__DIR__, 2) . '/composer.json';
+        $composerData = json_decode(file_get_contents($composerPath), true);
+        return [...explode('.', $composerData['version'] ?? '0.0.0'), ...[4, 0]];
     }
 }
