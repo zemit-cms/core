7328230e63b86ae33be3c3d47b741cbc112deae5
Julien Turbide <moi@jturbide.com>
2024-02-28 14:58:19 -0500

    fix typings

--- src/Modules/Frontend/Controllers/AbstractController.php
+++ src/Modules/Frontend/Controllers/AbstractController.php
@@ -22,10 +22,10 @@ abstract class AbstractController extends Controller
         Tag::setTitle($this->config->path('core.name', 'Zemit Frontend'));
         
         $this->assets->collection('head')
-            ->addCss('/head.css', true, true, [], true, true)
-            ->addJs('/head.js', true, true, [], true, true);
+            ->addCss('/head.css', true, true, [], null, true)
+            ->addJs('/head.js', true, true, [], null, true);
 
         $this->assets->collection('footer')
-            ->addJs('/footer.js', true, true, [], true, true);
+            ->addJs('/footer.js', true, true, [], null, true);
     }
 }
