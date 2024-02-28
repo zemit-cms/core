a69a2b23bb858580caf14f932661fa887443b4c2
Julien Turbide <moi@jturbide.com>
2024-02-28 16:15:25 -0500

    new abstract blameable

---
+++ src/Mvc/Model/AbstractTrait/AbstractBlameable.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Mvc\Model\AbstractTrait;
+
+use Phalcon\Mvc\Model\BehaviorInterface;
+use Phalcon\Mvc\Model\Relation;
+
+trait AbstractBlameable
+{
+    abstract public function addUserRelationship(
+        string $field = 'userId',
+        string $alias = 'UserEntity',
+        array $params = [],
+        string $ref = 'id',
+        string $type = 'belongsTo',
+        ?string $class = null
+    ): ?Relation;
+}
