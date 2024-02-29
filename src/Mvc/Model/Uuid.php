378563a45c416be7a18af5f9814b200dceb604c8
Julien Turbide <moi@jturbide.com>
2024-02-29 14:51:41 -0500

    improving model uuid trait for now

--- src/Mvc/Model/Uuid.php
+++ src/Mvc/Model/Uuid.php
@@ -13,6 +13,7 @@ namespace Zemit\Mvc\Model;
 
 use Phalcon\Db\RawValue;
 use Phalcon\Encryption\Security;
+use Phalcon\Mvc\EntityInterface;
 use Zemit\Mvc\Model\AbstractTrait\AbstractBehavior;
 use Zemit\Mvc\Model\AbstractTrait\AbstractInjectable;
 use Zemit\Mvc\Model\Behavior\Transformable;
@@ -40,8 +41,8 @@ trait Uuid
         
         $this->setUuidBehavior(new Transformable([
             'beforeValidationOnCreate' => [
-                $field => function ($model, $field) use ($security, $native, $binary) {
-                    return $model->getAttribute($field) ?? $native
+                $field => function (EntityInterface $model, string $field) use ($security, $native, $binary) {
+                    return $model->readAttribute($field) ?? $native
                         ? ($binary
                             ? new RawValue('UUID_TO_BIN(UUID())')
                             : new RawValue('UUID()')
@@ -52,35 +53,42 @@ trait Uuid
                         );
                 },
             ],
-            'afterFetch' => [
-                $field => function ($model, $field) use ($binary) {
-                    // $native not yet supported while fetching
-                    $value = $model->getAttribute($field);
-                    if ($binary && !empty($value)) {
-                        $hex = bin2hex($value);
-                        $uuid = sprintf(
-                            '%s-%s-%s-%s-%s',
-                            substr($hex, 0, 8),
-                            substr($hex, 8, 4),
-                            substr($hex, 12, 4),
-                            substr($hex, 16, 4),
-                            substr($hex, 20, 12)
-                        );
-                        $this->setAttribute($field, $uuid);
-                        return $uuid;
-                    }
-                },
-            ],
+//            'beforeValidation' => [
+//                $field => function (EntityInterface $model, string $field) use ($security, $binary) {
+//                    if ($binary) {
+//                        // uuid field should remain unchanged
+//                        return new RawValue('`' . $field . '`');
+//                    }
+//                },
+//            ],
+//            'afterFetch' => [
+//                $field => function (EntityInterface $model, string $field) use ($binary) {
+//                    // $native not yet supported while fetching
+//                    $value = $model->readAttribute($field);
+//                    if ($binary && !empty($value)) {
+//                        $hex = bin2hex($value);
+//                        return sprintf(
+//                            '%s-%s-%s-%s-%s',
+//                            substr($hex, 0, 8),
+//                            substr($hex, 8, 4),
+//                            substr($hex, 12, 4),
+//                            substr($hex, 16, 4),
+//                            substr($hex, 20, 12)
+//                        );
+//                    }
+//                    return $value;
+//                },
+//            ],
         ]));
     }
     
     /**
-     * Get a binary representation of UUID
+     * Get Binary Uuid
      *
-     * @param string $uuid
-     * @return string
+     * @param string $uuid The UUID string to convert to binary representation
+     * @return string The binary representation of the given UUID
      */
-    private function getBinaryUuid($uuid)
+    private function getBinaryUuid(string $uuid): string
     {
         return pack('h*', str_replace('-', '', $uuid));
     }
