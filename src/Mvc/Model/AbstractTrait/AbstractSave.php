0e08b52555263964183c37050452a494a036a42f
Julien Turbide <moi@jturbide.com>
2024-03-01 10:22:59 -0500

    new abstract save

---
+++ src/Mvc/Model/AbstractTrait/AbstractSave.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Mvc\Model\AbstractTrait;
+
+trait AbstractSave
+{
+    abstract public function save(): bool;
+}
