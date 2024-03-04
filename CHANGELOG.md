6d2af9772f5129a5609a2eb2da1d25e50de4a366
Julien Turbide <moi@jturbide.com>
2024-03-04 17:30:02 -0500

    resources migrations

---
+++ CHANGELOG.md
@@ -0,0 +1,36 @@
+# Changelog
+
+## [2.0.0](https://github.com/zemit-cms/core/releases/tag/1.0.0) (Planned)
+
+### Planned
+
+- New Restful system (v2)
+- Support for PHP 8.3+
+
+## [1.0.0](https://github.com/zemit-cms/core/releases/tag/1.0.0) (2024-03-04)
+
+### Changed
+
+- Strict PHP Typings
+- Bump support to PHP 8.2+
+- Bump support to Phalcon 5.6.1+
+- Stable Release 1.0.0
+
+### Added
+
+- Changelog Tracking for stable releases
+- Scaffolding Rest Controllers, Services, Models, Abstracts, Interfaces from database
+- Zemit Vendor bin file `./vendor/bin/zemit`
+
+### Fixed
+
+- Strict PHP Typings
+- License Stamping
+- Scrutinizer Build
+- Developer Tool
+- Unit tests
+
+### Removed
+
+- Unused Service Providers
+- Deprecated classes and methods
---
+++ resources/migrations/1.0.0/oauth2.php
@@ -0,0 +1,274 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Exception;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class Oauth2Migration_100
+ */
+class Oauth2Migration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function morph(): void
+    {
+        $this->morphTable('oauth2', [
+            'columns' => [
+                new Column(
+                    'id',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 1,
+                        'first' => true
+                    ]
+                ),
+                new Column(
+                    'user_id',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'id'
+                    ]
+                ),
+                new Column(
+                    'provider',
+                    [
+                        'type' => Column::TYPE_ENUM,
+                        'notNull' => true,
+                        'size' => "'google','microsoft'",
+                        'after' => 'user_id'
+                    ]
+                ),
+                new Column(
+                    'provider_uuid',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 120,
+                        'after' => 'provider'
+                    ]
+                ),
+                new Column(
+                    'access_token',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 120,
+                        'after' => 'provider_uuid'
+                    ]
+                ),
+                new Column(
+                    'refresh_token',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => false,
+                        'size' => 120,
+                        'after' => 'access_token'
+                    ]
+                ),
+                new Column(
+                    'email',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => false,
+                        'size' => 320,
+                        'after' => 'refresh_token'
+                    ]
+                ),
+                new Column(
+                    'name',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => false,
+                        'size' => 120,
+                        'after' => 'email'
+                    ]
+                ),
+                new Column(
+                    'first_name',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => false,
+                        'size' => 60,
+                        'after' => 'name'
+                    ]
+                ),
+                new Column(
+                    'last_name',
+                    [
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => false,
+                        'size' => 60,
+                        'after' => 'first_name'
+                    ]
+                ),
+                new Column(
+                    'meta',
+                    [
+                        'type' => Column::TYPE_JSON,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'last_name'
+                    ]
+                ),
+                new Column(
+                    'deleted',
+                    [
+                        'type' => Column::TYPE_TINYINTEGER,
+                        'default' => "0",
+                        'unsigned' => true,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'meta'
+                    ]
+                ),
+                new Column(
+                    'created_at',
+                    [
+                        'type' => Column::TYPE_DATETIME,
+                        'default' => "CURRENT_TIMESTAMP",
+                        'notNull' => true,
+                        'after' => 'deleted'
+                    ]
+                ),
+                new Column(
+                    'created_by',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'created_at'
+                    ]
+                ),
+                new Column(
+                    'created_as',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'created_by'
+                    ]
+                ),
+                new Column(
+                    'updated_at',
+                    [
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => false,
+                        'after' => 'created_as'
+                    ]
+                ),
+                new Column(
+                    'updated_by',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'updated_at'
+                    ]
+                ),
+                new Column(
+                    'updated_as',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'updated_by'
+                    ]
+                ),
+                new Column(
+                    'deleted_at',
+                    [
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => false,
+                        'after' => 'updated_as'
+                    ]
+                ),
+                new Column(
+                    'deleted_as',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'deleted_at'
+                    ]
+                ),
+                new Column(
+                    'deleted_by',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'deleted_as'
+                    ]
+                ),
+                new Column(
+                    'restored_at',
+                    [
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => false,
+                        'after' => 'deleted_by'
+                    ]
+                ),
+                new Column(
+                    'restored_by',
+                    [
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => false,
+                        'size' => 1,
+                        'after' => 'restored_at'
+                    ]
+                ),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY'),
+                new Index('id_UNIQUE', ['id'], 'UNIQUE'),
+                new Index('token_UNIQUE', ['access_token'], 'UNIQUE'),
+                new Index('provider_uuid_UNIQUE', ['provider_uuid'], 'UNIQUE'),
+                new Index('refresh_token_UNIQUE', ['refresh_token'], 'UNIQUE'),
+            ],
+            'options' => [
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8mb4_general_ci',
+            ],
+        ]);
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+    }
+}
