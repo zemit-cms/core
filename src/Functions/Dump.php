d73d436e0521cfdf88edf34199768bd5c4087bbe
Julien Turbide <moi@jturbide.com>
2024-03-05 02:03:35 -0500

    loading files before vendors improving dump function

--- src/Functions/Dump.php
+++ src/Functions/Dump.php
@@ -1,21 +1,50 @@
 <?php
 
-use Phalcon\Debug\Dump;
+use Phalcon\Support\Debug\Dump;
 
-if (!function_exists('vdd')) {
+if (!function_exists('dump')) {
     /**
      * Dump the passed variables and end the script.
+     * @param mixed ...$params The variables to be dumped.
+     * @return void
      */
-    function vdd(...$params): void
+    function dump(...$params): void
     {
-        var_dump(...$params);
-        exit_500();
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true)) {
+            echo json_encode($params, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
+        } else {
+            $dump = (new Dump([], true));
+            foreach ($params as $param) {
+                echo $dump->variable($param);
+            }
+        }
+    }
+}
+
+if (!function_exists('exit_500')) {
+    /**
+     * Terminate execution with a 500 Internal Server Error response code.
+     *
+     * This function sets the response headers to indicate a 500 Internal Server Error
+     * and terminates the execution of the script. It should be used when an unrecoverable
+     * error occurs and the server cannot fulfill the request.
+     *
+     * @return void
+     */
+    function exit_500(): void
+    {
+        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+            header('500 Internal Server Error');
+        }
+        exit(1);
     }
 }
 
 if (!function_exists('dd')) {
     /**
-     * Dump the passed variables and end the script.
+     * Dump variables and terminate execution with an error 500.
+     * @param mixed ...$params The variables to be dumped.
+     * @return void
      */
     function dd(...$params): void
     {
@@ -24,31 +53,15 @@ if (!function_exists('dd')) {
     }
 }
 
-if (!function_exists('dump')) {
+if (!function_exists('vdd')) {
     /**
-     * Dump the passed variables without ending the script.
+     * var_dump variables and terminate execution.
+     * @param mixed ...$params The variables to be dumped.
+     * @return void
      */
-    function dump(...$params): void
+    function vdd(...$params): void
     {
-        foreach ($params as $param) {
-            $ret = (new Dump([], true))->variable($param);
-            if (PHP_SAPI === 'cli') {
-                $ret = strip_tags($ret) . PHP_EOL;
-            }
-            echo $ret;
-        }
-    }
-}
-
-if (!function_exists('exit_500')) {
-    /**
-     * Exit and set header
-     */
-    function exit_500(): void
-    {
-        if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
-            header('500 Internal Server Error');
-        }
-        exit(1);
+        var_dump(...$params);
+        exit_500();
     }
 }
