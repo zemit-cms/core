5cf3e7405f069466e9402422a0e36f52c19720f4
Julien Turbide <moi@jturbide.com>
2024-03-13 01:52:04 -0400

    improving typings

--- src/Mvc/Controller/Traits/Query/Conditions/SoftDeleteConditions.php
+++ src/Mvc/Controller/Traits/Query/Conditions/SoftDeleteConditions.php
@@ -14,10 +14,12 @@ namespace Zemit\Mvc\Controller\Traits\Query\Conditions;
 use Phalcon\Db\Column;
 use Phalcon\Support\Collection;
 use Zemit\Mvc\Controller\Traits\Abstracts\AbstractModel;
+use Zemit\Mvc\Controller\Traits\Abstracts\AbstractQuery;
 
 trait SoftDeleteConditions
 {
     use AbstractModel;
+    use AbstractQuery;
     
     protected ?Collection $softDeleteConditions;
     
