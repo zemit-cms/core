e61211b27e65ce89911aaa0dd7b7b417f440e4d0
Julien Turbide <moi@jturbide.com>
2024-03-15 00:30:09 -0400

    license stamping

--- resources/migrations/1.0.0/flag.php
+++ resources/migrations/1.0.0/flag.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
 use Phalcon\Db\Column;
 use Phalcon\Db\Exception;
 use Phalcon\Db\Index;
