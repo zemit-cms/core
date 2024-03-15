dbe533ea41ab5dd1cc8eabc8ae55bae81051718f
Julien Turbide <moi@jturbide.com>
2024-03-15 03:16:27 -0400

    new bins & license stamping

--- bin/migration-run.sh
+++ bin/migration-run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+#
+# This file is part of the Zemit Framework.
+#
+# (c) Zemit Team <contact@zemit.com>
+#
+# For the full copyright and license information, please view the LICENSE.txt
+# file that was distributed with this source code.
+#
 ./vendor/bin/phalcon migration run --config=./devtools.php --directory=./ --migrations=./resources/migrations/ --no-auto-increment --force --verbose --log-in-db "$@"
--- bin/phpstan.sh
+++ bin/phpstan.sh
@@ -7,4 +7,5 @@
 # For the full copyright and license information, please view the LICENSE.txt
 # file that was distributed with this source code.
 #
-find ./src/ -type f -name "*.php" -exec grep -LZ "This file is part of the Zemit Framework." {} + | xargs -0 -n1 echo
+
+php -d xdebug.mode=develop ./vendor/bin/phpstan "$@"
