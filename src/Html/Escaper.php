cc6337b326cc18b32af5ccc51dae61017479d58d
Julien Turbide <moi@jturbide.com>
2024-03-17 18:46:03 -0400

    html escaper json test

--- src/Html/Escaper.php
+++ src/Html/Escaper.php
@@ -11,21 +11,37 @@
 
 namespace Zemit\Html;
 
+use Zemit\Html\Escaper\EscaperInterface;
+
 /**
- * {@inheritDoc}
+ * Zemit\Html\Escaper
+ *
+ * Escapes different kinds of text securing them. By using this component you
+ * may prevent XSS attacks.
+ *
+ * This component only works with UTF-8. The PREG extension needs to be compiled
+ * with UTF-8 support.
+ *
+ * ```php
+ * $escaper = new \Phalcon\Html\Escaper();
+ *
+ * $escaped = $escaper->escapeCss("font-family: <Verdana>");
+ *
+ * echo $escaped; // font\2D family\3A \20 \3C Verdana\3E
+ * ```
  */
-class Escaper extends \Phalcon\Html\Escaper
+class Escaper extends \Phalcon\Html\Escaper implements EscaperInterface
 {
     /**
      * Escapes a JSON string by raw URL encoding it.
      *
      *  JS side could decode and parse this way:
-     *  JSON.parse(decodeURIComponent('<?= $this->escaper->escapeJson([]);?>'));
+     *  JSON.parse(decodeURIComponent('<?= $this->escaper->json([]);?>'));
      *
      * @param mixed|null $json The JSON string to escape. If null, an empty string is escaped.
      * @return string Returns the raw URL encoded JSON string.
      */
-    public function escapeJson(mixed $json = null): string
+    public function json(mixed $json = null): string
     {
         if (is_null($json)) {
             return 'null';
---
+++ tests/Unit/Html/EscaperTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Unit\Html;
+
+use Zemit\Tests\Unit\AbstractUnit;
+
+class EscaperTest extends AbstractUnit
+{
+    public \Zemit\Html\Escaper\EscaperInterface $escaper;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->escaper = $this->di->get('escaper');
+    }
+    
+    public function testRequestFromDi(): void
+    {
+        $this->assertInstanceOf(\Phalcon\Html\Escaper::class, $this->escaper);
+        $this->assertInstanceOf(\Phalcon\Html\Escaper\EscaperInterface::class, $this->escaper);
+        
+        $this->assertInstanceOf(\Zemit\Html\Escaper::class, $this->escaper);
+        $this->assertInstanceOf(\Zemit\Html\Escaper\EscaperInterface::class, $this->escaper);
+    }
+    
+    /**
+     * Test the JSON method on the Escaper class when providing null as input.
+     * The method should return 'null' when null is passed as input.
+     */
+    public function testJsonWithNull(): void
+    {
+        $result = $this->escaper->json(json_encode(null));
+        
+        $this->assertSame('null', $result);
+    }
+    
+    /**
+     * Test the JSON method on the Escaper class when providing an array as input.
+     * The method should return raw URL encoded JSON string when an array is passed as input.
+     */
+    public function testJsonWithArray(): void
+    {
+        $arrayData = ['data' => 'Some Test Data'];
+        $result = $this->escaper->json(json_encode($arrayData));
+        
+        $this->assertSame(rawurlencode(json_encode($arrayData)), $result);
+    }
+    
+    /**
+     * Test the JSON method on the Escaper class when providing an object as input.
+     * The method should return raw URL encoded JSON string when an object is passed as input.
+     */
+    public function testJsonWithObject(): void
+    {
+        $objectData = new \stdClass();
+        $objectData->data = 'Some Test Data';
+        $result = $this->escaper->json(json_encode($objectData));
+        
+        $this->assertSame(rawurlencode(json_encode($objectData)), $result);
+    }
+}
