78dc595da1af5fadbdc08a2acc6e23008c47c06a
Julien Turbide <moi@jturbide.com>
2024-03-17 18:06:42 -0400

    test cli console

---
+++ tests/Unit/Cli/ConsoleTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Unit\Cli;
+
+use Zemit\Bootstrap;
+use Zemit\Tests\Unit\AbstractUnit;
+
+class ConsoleTest extends AbstractUnit
+{
+    public \Zemit\Cli\Console $console;
+    
+    protected string $mode = Bootstrap::MODE_CLI;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->console = $this->di->get('console');
+    }
+    
+    public function testConsoleFromDi(): void
+    {
+        $this->assertInstanceOf(\Phalcon\Cli\Console::class, $this->console);
+        $this->assertInstanceOf(\Zemit\Cli\Console::class, $this->console);
+    }
+}
