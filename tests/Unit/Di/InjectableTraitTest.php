b046e9ca940cac94ca43470080ef16e02bf2ed05
Julien Turbide <moi@jturbide.com>
2024-03-18 04:48:54 -0400

    improving injectable trait test

--- tests/Unit/Di/InjectableTraitTest.php
+++ tests/Unit/Di/InjectableTraitTest.php
@@ -13,9 +13,13 @@ declare(strict_types=1);
 
 namespace Unit\Di;
 
+use Phalcon\Di\Di;
 use Phalcon\Di\DiInterface;
 use Phalcon\Di\InjectionAwareInterface;
 use Phalcon\Flash\Session;
+use Phalcon\Session\Bag;
+use Phalcon\Session\BagInterface;
+use PHPUnit\Framework\TestStatus\Notice;
 use Zemit\Bootstrap;
 use Zemit\Di\InjectableTrait;
 use Zemit\Tests\Unit\AbstractUnit;
@@ -48,16 +52,31 @@ class InjectableTraitTest extends AbstractUnit
         // bootstrap should be defined
         $this->assertInstanceOf(Bootstrap::class, $this->injectable->bootstrap);
         
+        // persistent should return sessionBag
+        $this->di->set('sessionBag', new Bag($this->di->get('session'), 'default'));
+        $this->assertInstanceOf(BagInterface::class, $this->injectable->persistent);
+    }
+    
+    public function testThrowNonExistingSessionBag(): void
+    {
         // unset sessionBag and persistent should trigger an exception
         $this->expectException(\Phalcon\Di\Exception::class);
         $this->assertNull($this->injectable->persistent);
-        
+    }
+    
+    public function testThrowNonExistingProperty(): void
+    {
         // unset unknown should trigger an exception
-        $this->expectException(\Phalcon\Di\Exception::class);
-        $this->assertNull($this->injectable->nonExistingProperty);
-        
-        // persistent should return sessionBag
-        $this->di->set('sessionBag', new Session());
-        $this->assertInstanceOf(Session::class, $this->injectable->persistent);
+//        $this->expectException(Notice::class); // @todo ?
+        $this->assertNull(@$this->injectable->nonExistingProperty);
+    }
+    
+    public function testThrowNonExistingPropertyStrict(): void
+    {
+        $strictInjectable = new class implements InjectionAwareInterface {
+            use InjectableTrait;
+        };
+        $strictInjectable->setDI(new Di());
+        $this->assertNull(@$strictInjectable->__get('nonExistingProperty'));
     }
 }
