4a559db1ab20763b3dfc7f6698c0c4394006f48c
Julien Turbide <moi@jturbide.com>
2024-03-18 04:26:42 -0400

    locale en fr unit test

--- src/Locales/Fr.php
+++ src/Locales/Fr.php
@@ -16,11 +16,8 @@ use Phalcon\Translate\InterpolatorFactory;
 
 class Fr extends NativeArray
 {
-    public function __construct(InterpolatorFactory $interpolator, array $options)
+    public function __construct(InterpolatorFactory $interpolator, array $options = [])
     {
-        $this->replacePlaceholders('zemit', [
-            'zemit' => '<a href="https://www.zemit.com/">Zemit</a>'
-        ]);
         parent::__construct($interpolator, array_merge_recursive([
             'locale' => 'fr_CA.UTF-8',
             'defaultDomain' => 'zemit',
---
+++ tests/Unit/Locales/EnTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Unit\Locales;
+
+use Phalcon\Translate\InterpolatorFactory;
+use Zemit\Locales\En;
+use Zemit\Tests\Unit\AbstractUnit;
+
+class EnTest extends AbstractUnit
+{
+    public function testNativeArray(): void
+    {
+        $interpolationFactory = new InterpolatorFactory();
+        
+        $content = ['new' => 'value'];
+        $options = ['content' => $content];
+        
+        $en = new En($interpolationFactory, $options);
+        
+        $zemitLink = '<a href="https://www.zemit.com/">Zemit</a>';
+        $this->assertEquals('value', $en->t('new'));
+        $this->assertEquals('non-existing-key', $en->t('non-existing-key'));
+        $this->assertEquals("Powered by $zemitLink.", $en->t('powered-by', ['zemit' => $zemitLink]));
+    }
+}
