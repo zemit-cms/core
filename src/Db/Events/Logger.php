0c45cd3998ae37975d64a7e5bdc01f18d85ecd1c
Julien Turbide <moi@jturbide.com>
2024-03-19 14:43:20 -0400

    fix infinite loop db logger when session is using db

--- src/Db/Events/Logger.php
+++ src/Db/Events/Logger.php
@@ -21,6 +21,8 @@ use Zemit\Di\Injectable;
  */
 class Logger extends Injectable
 {
+    public bool $inProgress = false;
+    
     /**
      * Executes before a database query is executed.
      *
@@ -33,26 +35,30 @@ class Logger extends Injectable
     {
         if ($this->config->path('logger.enable') || $this->config->path('app.logger')) {
             if ($this->config->path('loggers.database.enable')) {
-                $sessionId = $this->identity->getSession()?->getId();
-                $userId = $this->identity->getUserId() ?: null;
-                $userAsId = $this->identity->getUserAsId() ?: null;
-                
-                $log = json_encode([
-                    'type' => 'query',
-                    'sessionId' => $sessionId,
-                    'userId' => $userId,
-                    'userAsId' => $userAsId,
-                    'event' => [
-                        'type' => $event->getType(),
-                        'data' => $event->getData(),
-                    ],
-                    'meta' => [
-                        'sqlStatement' => $connection->getSQLStatement(),
-                        'sqlVariables' => $connection->getSQLVariables(),
-                    ],
-                ]);
-                
-                $this->loggers->get('database')->info($log);
+                if (!$this->inProgress) {
+                    $this->inProgress = true;
+                    $sessionId = $this->identity->getSession()?->getId();
+                    $userId = $this->identity->getUserId() ?: null;
+                    $userAsId = $this->identity->getUserAsId() ?: null;
+                    
+                    $log = json_encode([
+                        'type' => 'query',
+                        'sessionId' => $sessionId,
+                        'userId' => $userId,
+                        'userAsId' => $userAsId,
+                        'event' => [
+                            'type' => $event->getType(),
+                            'data' => $event->getData(),
+                        ],
+                        'meta' => [
+                            'sqlStatement' => $connection->getSQLStatement(),
+                            'sqlVariables' => $connection->getSQLVariables(),
+                        ],
+                    ]);
+                    
+                    $this->loggers->get('database')->info($log);
+                    $this->inProgress = false;
+                }
             }
         }
     }
