99304eb73a0b23c5ec27ec52a47bd8a37b479861
Julien Turbide <moi@jturbide.com>
2024-03-31 19:40:01 -0400

    fix to hmvc

--- src/Mvc/Application.php
+++ src/Mvc/Application.php
@@ -33,17 +33,21 @@ class Application extends \Phalcon\Mvc\Application
         $di->setShared('application', $this);
         parent::__construct($di);
     }
-
+    
     /**
-     * HMVC request
-     * You can request call any module/namespace
+     * Requests a location using the specified dispatcher.
+     *
+     * @param array $location An array containing location information, including namespace, module, controller,
+     *                        action, and params. Default is an empty array.
+     * @return string The response content of the requested location.
      * @throws \Exception
      */
     public function request(array $location = []): string
     {
         // Get a unique dispatcher
-        $dispatcher = clone $this->getDI()->get('dispatcher');
+        $dispatcher = $this->getDI()->get('dispatcher');
         assert($dispatcher instanceof AbstractDispatcher);
+        $dispatcher = clone $dispatcher;
         
         // Route dispatcher
         $dispatcher->setDefaultNamespace($location['namespace'] ?? $dispatcher->getNamespaceName());
@@ -53,7 +57,7 @@ class Application extends \Phalcon\Mvc\Application
             $dispatcher->setControllerName($location['controller'] ?? 'index');
         }
         elseif ($dispatcher instanceof CliDispatcher) {
-            $dispatcher->setTaskName($location['task'] ?? 'index');
+            $dispatcher->setTaskName($location['task'] ?? 'main');
         }
         $dispatcher->setActionName($location['action'] ?? 'index');
         $dispatcher->setParams($location['params'] ?? []);
