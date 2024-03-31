39ee3c5830e2dc24d2fe3ab9e76e72cef64c14ee
Julien Turbide <moi@jturbide.com>
2024-03-31 19:34:15 -0400

    added new Zemit Loader class

---
+++ src/Autoload/Loader.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Autoload;
+
+use Phalcon\Autoload\Exception;
+
+/**
+ * Class Loader
+ *
+ * This class extends the \Phalcon\Autoload\Loader class and disable file checking callback for better performance.
+ *
+ * @throws Exception Throws an exception if initialization fails.
+ */
+class Loader extends \Phalcon\Autoload\Loader
+{
+    /**
+     * @throws Exception
+     */
+    public function __construct(bool $isDebug = false)
+    {
+        parent::__construct($isDebug);
+        
+        // Do not check file existence.
+        $this->setFileCheckingCallback(null);
+    }
+}
