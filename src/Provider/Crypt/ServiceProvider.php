24bff48896012f1f06b51597d604f20c5ca77fc0
Julien Turbide <moi@jturbide.com>
2024-04-17 13:43:20 -0400

    updating crypt service to use new settings

--- src/Provider/Crypt/ServiceProvider.php
+++ src/Provider/Crypt/ServiceProvider.php
@@ -23,15 +23,34 @@ class ServiceProvider extends AbstractServiceProvider
     public function register(DiInterface $di): void
     {
         $di->setShared($this->getName(), function (?string $cipher = null, ?bool $useSigning = null) use ($di) {
-    
+            
             $config = $di->get('config');
             assert($config instanceof ConfigInterface);
             $options = $config->pathToArray('crypt', []);
             
             $cipher ??= $options['cipher'] ?? 'aes-256-cfb';
-            $useSigning ??= $options['useSigning'] ?? false;
+            $useSigning ??= $options['useSigning'] ?? true;
             
-            return new Crypt($cipher, $useSigning);
+            $hash = $options['hash'] ?? 'sha256';
+            $key = $options['key'] ?? 'T4\xb1\x8d\xa9\x98\x05\\x8c\xbe\x1d\x07&[\x99\x18\xa4~Lc1\xbeW\xb3';
+            $paddingScheme = $options['paddingScheme'] ?? Crypt::PADDING_DEFAULT;
+            $padFactoryClass = $options['padFactory'] ?? \Phalcon\Encryption\Crypt\PadFactory::class;
+            $authData = $options['authData'] ?? '';
+            $authTag = $options['authTag'] ?? '';
+            $authTagLength = $options['authTagLength'] ?? 16;
+            
+            $padFactory = new $padFactoryClass();
+            assert($padFactory instanceof \Phalcon\Encryption\Crypt\PadFactory);
+            
+            $crypt = new Crypt($cipher, $useSigning, $padFactory);
+            $crypt->setHashAlgorithm($hash);
+            $crypt->setPadding($paddingScheme);
+            $crypt->setKey($key);
+            $crypt->setAuthData($authData);
+            $crypt->setAuthTag($authTag);
+            $crypt->setAuthTagLength($authTagLength);
+            
+            return $crypt;
         });
     }
 }
