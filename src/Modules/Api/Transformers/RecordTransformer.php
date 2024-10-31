102740f65aac5b2c257045473512ba7371de743c
Julien Turbide <moi@jturbide.com>
2024-10-31 10:12:22 -0400

    record transformer

---
+++ src/Modules/Api/Transformers/RecordTransformer.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Modules\Api\Transformers;
+
+use Zemit\Fractal\Transformer;
+use Zemit\Models\Record;
+
+class RecordTransformer extends Transformer
+{
+    public function transform(?Record $record)
+    {
+        return $record? $record->toArray() : [];
+    }
+}
