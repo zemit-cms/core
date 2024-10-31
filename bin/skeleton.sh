5387b5f7ddf0ea4d2a7c8888b240a696a35b2f03
Julien Turbide <moi@jturbide.com>
2024-10-31 11:11:33 -0400

    added psd skeleton bin

---
+++ bin/skeleton.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This file is part of the Zemit Framework.
+#
+# (c) Zemit Team <contact@zemit.com>
+#
+# For the full copyright and license information, please view the LICENSE.txt
+# file that was distributed with this source code.
+#
+
+php -d xdebug.mode=off ./vendor/bin/pds-skeleton "$@"
