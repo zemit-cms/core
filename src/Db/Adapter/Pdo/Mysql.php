e8ab34ebb0e53f302b1405227128291cafcfd9bd
Julien Turbide <moi@jturbide.com>
2024-11-02 14:24:38 -0400

    remove fix because phalcon fixed this

--- src/Db/Adapter/Pdo/Mysql.php
+++ src/Db/Adapter/Pdo/Mysql.php
@@ -15,42 +15,4 @@ use Phalcon\Db\Column;
 
 class Mysql extends \Phalcon\Db\Adapter\Pdo\Mysql
 {
-    public function describeColumns(string $table, string $schema = null): array
-    {
-        $definitions = parent::describeColumns($table, $schema);
-        
-        // @todo see if we can remove this and reactivate phpstan
-        // @phpstan-ignore-next-line
-        if (Column::TYPE_TINYINTEGER !== Column::TYPE_BINARY) {
-            return $definitions;
-        }
-        
-        // @phpstan-ignore-next-line
-        foreach ($definitions as $definitionKey => $definition) {
-            
-            if ($definition->getType() === Column::TYPE_TINYINTEGER && !$definition->isNumeric()) {
-                // probably a binary at this point
-                
-                $newDefinition = [];
-                
-                // protected to public
-                $prefix = chr(0) . '*' . chr(0);
-                foreach ((array)$definition as $key => $value) {
-                    $newDefinition[str_replace($prefix, '', $key)] = $value;
-                }
-                
-                $newDefinition['bindType'] = Column::BIND_PARAM_BLOB;
-                $newDefinition['type'] = Column::TYPE_VARBINARY;
-                unset($newDefinition['scale']);
-                
-                /**
-                 * reset definition
-                 * @psalm-suppress InvalidArgument
-                 */
-                $definitions[$definitionKey] = new Column($definition->getName(), $newDefinition);
-            }
-        }
-        
-        return $definitions;
-    }
 }
