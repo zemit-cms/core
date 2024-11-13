9be643efa0a8cbf42ebc84eb0525fe8625e687dc
Julien Turbide <moi@jturbide.com>
2024-11-13 15:33:31 -0500

    new string helper methods - removeNonPrintable - normalizeLineBreaks - sanitizeUTF8

---
+++ src/Support/Helper/Str/RemoveNonPrintable.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Support\Helper\Str;
+
+/**
+ * Remove non-printable characters
+ */
+class RemoveNonPrintable
+{
+    public function __invoke(string $string, string $nonPrintableRegex = '[[:cntrl:]' . PHP_EOL . ']', string $replacement = ''): string
+    {
+        return mb_ereg_replace($nonPrintableRegex, $replacement, $string) ?: '';
+    }
+}
--- src/Support/HelperFactory.php
+++ src/Support/HelperFactory.php
@@ -12,6 +12,9 @@
 namespace Zemit\Support;
 
 use Zemit\Support\Helper\Arr\FlattenKeys;
+use Zemit\Support\Helper\Str\NormalizeLineBreaks;
+use Zemit\Support\Helper\Str\RemoveNonPrintable;
+use Zemit\Support\Helper\Str\SanitizeUTF8;
 use Zemit\Support\Helper\Str\Slugify;
 
 /**
@@ -23,6 +26,9 @@ use Zemit\Support\Helper\Str\Slugify;
  * # New methods
  * @method string flattenKeys(array $collection = [], string $delimiter = '.', bool $lowerKey = true)
  * @method string slugify(string $string, array $replace = [], string $delimiter = '-')
+ * @method string sanitizeUTF8(string $string)
+ * @method string removeNonPrintable(string $string, string $nonPrintableRegex = '[[:cntrl:]' . PHP_EOL . ']', string $replacement = '')
+ * @method string normalizeLineBreaks(string $string, string $nonPrintableRegex = "\r\n", string $replacement = "\r")
  */
 class HelperFactory extends \Phalcon\Support\HelperFactory
 {
@@ -36,6 +42,9 @@ class HelperFactory extends \Phalcon\Support\HelperFactory
         return array_merge(parent::getServices(), [
             'flattenKeys' => FlattenKeys::class,
             'slugify' => Slugify::class,
+            'sanitizeUTF8' => SanitizeUTF8::class,
+            'removeNonPrintable' => RemoveNonPrintable::class,
+            'normalizeLineBreaks' => NormalizeLineBreaks::class,
         ]);
     }
 }
