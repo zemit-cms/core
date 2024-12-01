9e59001c0cdf17f553494235e66d380c1410ac44
Julien Turbide <moi@jturbide.com>
2024-11-30 21:04:40 -0500

    scaffold run script

---
+++ bin/scaffold-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This file is part of the Zemit Framework.
+#
+# (c) Zemit Team <contact@zemit.com>
+#
+# For the full copyright and license information, please view the LICENSE.txt
+# file that was distributed with this source code.
+#
+./zemit cli scaffold run --namespace=Zemit "$@"
