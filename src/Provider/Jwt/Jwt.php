955f69e36920e0df2244f345a8d1d445e8ededa7
Julien Turbide <moi@jturbide.com>
2024-12-27 12:52:22 -0500

    JWT does not throw exception anymore, we are now returning errors

--- src/Provider/Jwt/Jwt.php
+++ src/Provider/Jwt/Jwt.php
@@ -126,9 +126,9 @@ class Jwt
     
     /**
      * Validate the token
-     * @throws ValidatorException
+     * @throws ValidatorException|\DateMalformedStringException
      */
-    public function validateToken(Token $token = null, int $timeShift = 0, array $options = [], AbstractSigner $signer = null): void
+    public function validateToken(Token $token = null, int $timeShift = 0, array $options = [], AbstractSigner $signer = null): array
     {
         $token ??= $this->token;
         $signer ??= $this->signer;
@@ -147,6 +147,8 @@ class Jwt
         $this->validator->validateExpiration($options['expiration']);
         $this->validator->validateIssuedAt($options['issuedAt']);
         $this->validator->validateSignature($signer, $options['passphrase']);
+        
+        return $this->validator->getErrors();
     }
     
     /**
