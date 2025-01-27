72b3a364391ffee52cc27aa3cefaef818b96102e
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-01-27 16:10:22 -0500

    improve audit detail

--- src/Mvc/Model/Behavior/Blameable.php
+++ src/Mvc/Model/Behavior/Blameable.php
@@ -121,9 +121,9 @@ class Blameable extends Behavior
             $auditDetail = new $auditDetailClass();
             assert($auditDetail instanceof AuditDetailInterface);
             
-            $auditDetail->setModel(get_class($model));
-            $auditDetail->setTable($model->getSource());
-            $auditDetail->setPrimary($model->readAttribute('id'));
+            $auditDetail->setModel($audit->getModel());
+            $auditDetail->setTable($audit->getTable());
+            $auditDetail->setPrimary($audit->getPrimary());
             $auditDetail->setEvent($event);
             $auditDetail->setColumn($column);
             $auditDetail->setMap($map);
