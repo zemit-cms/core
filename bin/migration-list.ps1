134868e1eb53808eaeb47aa0e5c28b261d7f348c
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-01-29 13:11:12 -0500

    adapt bin to powershell scripts

---
+++ bin/migration-list.ps1
@@ -0,0 +1,8 @@
+# This file is part of the Zemit Framework.
+# 
+# (c) Zemit Team <contact@zemit.com>
+# 
+# For the full copyright and license information, please view the LICENSE.txt
+# file that was distributed with this source code.
+
+php "./vendor/bin/phalcon" migration list --config="./devtools.php" --directory="./" --migrations="./resources/migrations/" --log-in-db $args
