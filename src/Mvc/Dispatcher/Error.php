5c0475e5d687749ebf452bf63c0d9eb9e489cf47
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-03-28 15:08:51 -0400

    Add handling for 401 unauthorized errors with validation
    Introduced handling for 401 errors triggered by ValidatorExceptions. If debugging is disabled, the dispatcher forwards to the configured unauthorized error route; otherwise, the standard error is thrown. This improves error management and ensures proper response behavior.

--- src/Mvc/Dispatcher/Error.php
+++ src/Mvc/Dispatcher/Error.php
@@ -13,6 +13,7 @@ namespace Zemit\Mvc\Dispatcher;
 
 use Exception;
 use Phalcon\Dispatcher\Exception as DispatchException;
+use Phalcon\Encryption\Security\JWT\Exceptions\ValidatorException;
 use Phalcon\Events\Event;
 use Zemit\Di\Injectable;
 use Zemit\Mvc\Dispatcher;
@@ -33,45 +34,65 @@ class Error extends Injectable
         'action' => 'fatal',
     ];
     
+    public array $defaultUnauthorizedRoute = [
+        'module' => null,
+        'namespace' => null,
+        'controller' => 'error',
+        'action' => 'unauthorized',
+    ];
+    
     /**
      * Forward to 404 or 500 error controller
      * @throws Exception
      */
     public function beforeException(Event $event, Dispatcher $dispatcher, Exception $exception): bool
     {
+        // Everything else, if debug is false, forward to fatal error 500
+        $appDebug = $this->config->path('app.debug', false);
+        $debugEnable = $this->config->path('debug.enable', false);
+        $isDebug = $appDebug || $debugEnable;
+        
         switch ($exception->getCode()) {
             
             case DispatchException::EXCEPTION_HANDLER_NOT_FOUND:
             case DispatchException::EXCEPTION_ACTION_NOT_FOUND:
                 if ($exception instanceof DispatchException) {
                     $route = $this->config->pathToArray('router.notFound') ?? [];
-                    
                     $this->appendDefaultToRoute($route, $this->defaultNotFoundRoute);
                     $route['params']['exception'] = $exception;
-                    
                     $dispatcher->forward($route, true);
                     return false;
                 }
                 break;
-            
+
+            case 401:
+                if ($exception instanceof ValidatorException) {
+                    http_response_code(401);
+                    
+                    if (!$isDebug) {
+                        $route = $this->config->pathToArray('router.unauthorized') ?? [];
+                        $this->appendDefaultToRoute($route, $this->defaultUnauthorizedRoute);
+                        $route['params']['exception'] = $exception;
+                        $dispatcher->forward($route, true);
+                        
+                        return false;
+                    }
+                }
+                break;
+                
             default:
                 http_response_code(500);
                 
-                // Everything else, if debug is false, forward to fatal error 500
-                $appDebug = $this->config->path('app.debug', false);
-                $debugEnable = $this->config->path('debug.enable', false);
-                
-                if (!$appDebug && !$debugEnable) {
+                if (!$isDebug) {
                     $route = $this->config->pathToArray('router.error') ?? [];
-                    
                     $this->appendDefaultToRoute($route, $this->defaultErrorRoute);
                     $route['params']['exception'] = $exception;
-                    
                     $dispatcher->forward($route, true);
                     return false;
                 }
                 break;
         }
+        
         throw $exception;
     }
     
