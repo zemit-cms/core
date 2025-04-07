0a345ca2d5f0df57dd95f697945b4c1769eac4c8
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-04-07 14:38:43 -0400

    Refine CORS header handling for better flexibility.
    Updated preflight logic to skip redundant 'Access-Control-Allow-Origin' header setting and ensure boolean headers are formatted as strings. Adjusted configuration to support multiple origins by parsing comma-separated values into arrays.

--- src/Mvc/Dispatcher/Preflight.php
+++ src/Mvc/Dispatcher/Preflight.php
@@ -41,15 +41,29 @@ class Preflight extends Injectable
         // Set cors headers
         foreach ($headers as $headerKey => $headerValue) {
             if (!$response->hasHeader($headerKey) && !is_array($headerValue)) {
+                
+                // ignore Access-Control-Allow-Origin as we will add the header after
+                if ($headerKey === 'Access-Control-Allow-Origin') {
+                    continue;
+                }
+                
+                // Ensure the bool values are sent as string
+                if (is_bool($headerValue)) {
+                    $headerValue = $headerValue ? 'true' : 'false';
+                }
+                
+                // set the header
                 $response->setHeader($headerKey, $headerValue);
             }
         }
         
         // Set origin value if origin is allowed
         $originKey = 'Access-Control-Allow-Origin';
-        if (!$response->hasHeader($originKey)
-            && is_array($headers[$originKey])
-            && in_array($origin, $headers[$originKey])
+        if (!$response->hasHeader($originKey) &&
+            ($headers[$originKey] === '*' || (
+                is_array($headers[$originKey]) &&
+                (in_array($origin, $headers[$originKey])) || in_array('*', $headers[$originKey]))
+            )
         ) {
             $response->setHeader($originKey, $origin);
         }
