788bb10d7bb0ea168a272d789ad5adc539337038
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-10-15 11:26:26 -0400

    add `RemoveSoftDeleteConditionsWhileFiltering` class to clear soft delete conditions based on filter fields param "deleted"

---
+++ src/Mvc/Controller/Behavior/Query/Conditions/RemoveSoftDeleteConditionsWhileFiltering.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Zemit Framework.
+ *
+ * (c) Zemit Team <contact@zemit.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Zemit\Mvc\Controller\Behavior\Query\Conditions;
+
+use Phalcon\Events\Event;
+use Phalcon\Filter\Exception;
+use Zemit\Mvc\Controller\Restful;
+
+class RemoveSoftDeleteConditionsWhileFiltering
+{
+    /**
+     * This method is executed after initializing conditions for the given controller.
+     * Clears the soft delete conditions for the controller if specific filter fields are provided.
+     *
+     * @param Event $event The event instance triggering the method.
+     * @param Restful $controller The controller instance on which conditions are being initialized.
+     * @return void
+     * @throws Exception
+     */
+    public function afterInitializeConditions(Event $event, Restful $controller): void
+    {
+        if ($controller->hasFiltersFieldsParams(['deleted'])) {
+            $controller->getSoftDeleteConditions()->clear();
+        }
+    }
+}
