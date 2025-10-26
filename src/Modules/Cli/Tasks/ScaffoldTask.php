57d964f3ca6458d5db7bbbd43232ad2464e1db20
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-10-26 02:49:17 -0400

    Remove unnecessary `(string)` cast in `fwrite` operation in `ScaffoldTask` for improved type safety and clarity

--- src/Modules/Cli/Tasks/ScaffoldTask.php
+++ src/Modules/Cli/Tasks/ScaffoldTask.php
@@ -1161,7 +1161,7 @@ PHP;
             return false; // Failed to open file
         }
         
-        $writeSuccess = fwrite($fileHandle, (string)$utf8Text) !== false;
+        $writeSuccess = fwrite($fileHandle, $utf8Text) !== false;
         $closeSuccess = fclose($fileHandle);
         
         return $writeSuccess && $closeSuccess;
