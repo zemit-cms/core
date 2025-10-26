e83aa56a533340cf281892bcbb1b030c8f968dd5
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-10-26 18:18:22 -0400

    Add detailed `@return` type annotation to `getVersion` method in `Version` for improved clarity and documentation.

--- src/Support/Version.php
+++ src/Support/Version.php
@@ -25,6 +25,8 @@ class Version extends \Phalcon\Support\Version
      * C - Min version (two digits)
      * D - Special release: 1 = Alpha, 2 = Beta, 3 = RC, 4 = Stable
      * E - Special release version i.e. RC1, Beta2 etc.
+     *
+     * @return array{int, int, int, int, int}
      */
     #[\Override]
     protected function getVersion(): array
