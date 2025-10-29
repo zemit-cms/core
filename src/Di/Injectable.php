c00d79cbbb5bf0f6b561655ce6d544612d56ca3f
Julien Turbide <4306188+jturbide@users.noreply.github.com>
2025-10-29 01:05:44 -0400

    Remove outdated PHPDoc annotations and reorganize properties in `InjectableProperties` and `Injectable` for clarity and maintainability.

--- src/Di/Injectable.php
+++ src/Di/Injectable.php
@@ -13,52 +13,6 @@ declare(strict_types=1);
 
 namespace Zemit\Di;
 
-/**
- * This class allows to access services in the services container by just only
- * accessing a public property with the same name of a registered service
- *
- * @property \Zemit\Mvc\Dispatcher|\Phalcon\Mvc\Dispatcher|\Phalcon\Mvc\DispatcherInterface $dispatcher
- * @property \Zemit\Mvc\Router|\Phalcon\Mvc\Router|\Phalcon\Mvc\RouterInterface $router
- * @property \Zemit\Mvc\Url|\Phalcon\Mvc\Url|\Phalcon\Mvc\Url\UrlInterface $url
- * @property \Zemit\Http\Response|\Phalcon\Http\Response|\Phalcon\Http\ResponseInterface $response
- * @property \Phalcon\Http\Response\Cookies|\Phalcon\Http\Response\CookiesInterface $cookies
- * @property \Zemit\Filter\Filter|\Phalcon\Filter\Filter $filter
- * @property \Phalcon\Flash\Direct $flash
- * @property \Phalcon\Flash\Session $flashSession
- * @property \Phalcon\Session\ManagerInterface $session
- * @property \Phalcon\Events\Manager|\Phalcon\Events\ManagerInterface $eventsManager
- * @property \Phalcon\Db\Adapter\AdapterInterface $db
- * @property \Phalcon\Db\Adapter\AdapterInterface $dbr
- * @property \Phalcon\Db\Adapter\AdapterInterface $dbd
- * @property \Phalcon\Encryption\Crypt|\Phalcon\Encryption\Crypt\CryptInterface $crypt
- * @property \Zemit\Html\Escaper|\Phalcon\Html\Escaper|\Phalcon\Html\Escaper\EscaperInterface $escaper
- * @property \Phalcon\Annotations\Adapter\Memory|\Phalcon\Annotations\Adapter\AbstractAdapter $annotations
- * @property \Zemit\Mvc\Model\Manager|\Phalcon\Mvc\Model\Manager|\Phalcon\Mvc\Model\ManagerInterface $modelsManager
- * @property \Phalcon\Mvc\Model\MetaData\Memory|\Phalcon\Mvc\Model\MetadataInterface $modelsMetadata
- * @property \Phalcon\Mvc\Model\Transaction\Manager|\Phalcon\Mvc\Model\Transaction\ManagerInterface $transactionManager
- * @property \Zemit\Assets\Manager|\Phalcon\Assets\Manager $assets
- * @property \Phalcon\Di\Di|\Phalcon\Di\DiInterface $di
- * @property \Phalcon\Session\Bag|\Phalcon\Session\BagInterface $persistent
- * @property \Zemit\Mvc\View|\Phalcon\Mvc\View|\Phalcon\Mvc\ViewInterface $view
- * 
- * @property \Zemit\Identity\Manager $identity
- * @property \Zemit\Locale $locale
- * @property \Zemit\Encryption\Security $security
- * @property \Zemit\Tag $tag
- * 
- * @property \Zemit\Acl\Acl|\Zemit\Acl\AclInterface $acl
- * @property \Zemit\Support\HelperFactory $helper
- * @property \Zemit\Bootstrap\Config $config
- * @property \Phalcon\Logger\Logger $logger
- * @property \Zemit\Logger\Loggers $loggers
- * @property \Zemit\Db\Profiler $profiler
- * @property \Zemit\Bootstrap $bootstrap
- * @property \Zemit\Provider\Jwt\Jwt $jwt
- * @property \Zemit\Http\Request|\Zemit\Http\RequestInterface $request
- * @property \Zemit\Support\Models $models
- * 
- * // @todo review Phalcon & Zemit DI
- */
 class Injectable extends \Phalcon\Di\Injectable implements \Phalcon\Di\InjectionAwareInterface
 {
     use InjectableProperties;
--- src/Di/InjectableProperties.php
+++ src/Di/InjectableProperties.php
@@ -14,68 +14,64 @@ declare(strict_types=1);
 namespace Zemit\Di;
 
 /**
- * @todo add missing
- * @todo make interface for everything
- * @todo make wrapper for externals
- * @todo make wrapper for phalcon
- *
- * @property \Phalcon\Di\FactoryDefault|\Phalcon\Di\Di|\Phalcon\Di\DiInterface $di
- * @property \Zemit\Bootstrap $bootstrap
  * @property \Zemit\Acl\AclInterface|\Zemit\Acl\Acl $acl
- * @property \Zemit\Mvc\Application $application
  * @property \Zemit\Assets\Manager $assets
- * @property \Phalcon\Annotations\Adapter\AdapterInterface $annotations
- * @property \Aws\Sdk $aws
- * @property \Zemit\Cache\Cache $cache
- * @property \Xenolope\Quahog\Client $clamav
+ * @property \Zemit\Bootstrap $bootstrap
  * @property \Zemit\Bootstrap\Config $config
+ * @property \Zemit\Cache\Cache $cache
  * @property \Zemit\Cli\Console $console
- * @property \Phalcon\Http\Response\Cookies $cookies
- * @property \Phalcon\Encryption\Crypt $crypt
- * @property \Phalcon\Db\Adapter\AbstractAdapter $db
- * @property \Phalcon\Db\Adapter\AbstractAdapter $dbr
- * @property \Phalcon\Db\Adapter\AbstractAdapter $dbd
  * @property \Zemit\Support\Debug $debug
  * @property \Zemit\Dispatcher\AbstractDispatcher $dispatcher
  * @property \Zemit\Support\Env $env
  * @property \Zemit\Html\Escaper $escaper
- * @property \Phalcon\Events\ManagerInterface $eventsManager
- * @property \League\Flysystem\Filesystem $fileSystem
  * @property \Zemit\Filter\Filter $filter
- * @property \Phalcon\Flash\FlashInterface $flash
-// * @property \Zemit\Support\Gravatar $gravatar
- * @property \Zemit\Support\Helper $helper
+ * @property \Zemit\Support\HelperFactory $helper
  * @property \Zemit\Identity\Manager $identity
- * @property \PhpImap\Mailbox $imap
- * @property \Zemit\Provider\Jwt\Jwt $jwt
  * @property \Zemit\Locale $locale
  * @property \Zemit\Logger\Loggers $loggers
- * @property \Phalcon\Logger\LoggerInterface $logger
- * @property \joshtronic\LoremIpsum $loremIpsum
- * @property \Phalcon\Incubator\Mailer\Manager $mailer
  * @property \Zemit\Support\Models $models
- * @property \Phalcon\Cache\CacheInterface $modelsCache
+ * @property \Zemit\Mvc\Application $application
  * @property \Zemit\Mvc\Model\ManagerInterface $modelsManager
+ * @property \Zemit\Db\Profiler $profiler
+ * @property \Zemit\Http\RequestInterface $request
+ * @property \Zemit\Http\ResponseInterface $response
+ * @property \Zemit\Mvc\Router $router
+ * @property \Zemit\Encryption\Security $security
+ * @property \Zemit\Tag $tag
+ * @property \Zemit\Support\Utils $utils
+ * @property \Zemit\Support\Version $version
+ * @property \Zemit\Mvc\Url $url
+ * @property \Zemit\Mvc\View $view
+ * @property \Zemit\Provider\Jwt\Jwt $jwt
+ *
+ * @property \Phalcon\Annotations\Adapter\AdapterInterface $annotations
+ * @property \Phalcon\Http\Response\Cookies $cookies
+ * @property \Phalcon\Encryption\Crypt $crypt
+ * @property \Phalcon\Db\Adapter\AbstractAdapter $db
+ * @property \Phalcon\Db\Adapter\AbstractAdapter $dbd
+ * @property \Phalcon\Db\Adapter\AbstractAdapter $dbr
+ * @property \Phalcon\Events\ManagerInterface $eventsManager
+ * @property \Phalcon\Flash\FlashInterface $flash
+ * @property \Phalcon\Logger\LoggerInterface $logger
+ * @property \Phalcon\Incubator\Mailer\Manager $mailer
+ * @property \Phalcon\Cache\CacheInterface $modelsCache
  * @property \Phalcon\Mvc\Model\MetaDataInterface $modelsMetadata
+ * @property \Phalcon\Session\ManagerInterface $session
+ * @property \Phalcon\Translate\Adapter\AdapterInterface $translate
+ * @property \Phalcon\Mvc\View\Engine\Volt $volt
+ * @property \Phalcon\Di\FactoryDefault|\Phalcon\Di\Di|\Phalcon\Di\DiInterface $di
+ *
+ * @property \Aws\Sdk $aws
+ * @property \League\Flysystem\Filesystem $fileSystem
  * @property \League\OAuth2\Client\Provider\GenericProvider $oauth2Client
  * @property \League\OAuth2\Client\Provider\Facebook $oauth2Facebook
  * @property \League\OAuth2\Client\Provider\Google $oauth2Google
- * @property \thiagoalessio\TesseractOCR\TesseractOCR $ocr
+ * @property \joshtronic\LoremIpsum $loremIpsum
  * @property \Orhanerday\OpenAi\OpenAi $openAi
- * @property \Zemit\Db\Profiler $profiler
+ * @property \PhpImap\Mailbox $imap
  * @property \ReCaptcha\ReCaptcha $reCaptcha
- * @property \Zemit\Http\RequestInterface $request
- * @property \Phalcon\Http\ResponseInterface $response
- * @property \Zemit\Mvc\Router $router
- * @property \Zemit\Encryption\Security $security
- * @property \Phalcon\Session\ManagerInterface $session
- * @property \Zemit\Tag $tag
- * @property \Phalcon\Translate\Adapter\AdapterInterface $translate
- * @property \Zemit\Mvc\Url $url
- * @property \Zemit\Support\Utils $utils
- * @property \Zemit\Support\Version $version
- * @property \Zemit\Mvc\View $view
- * @property \Phalcon\Mvc\View\Engine\Volt $volt
+ * @property \thiagoalessio\TesseractOCR\TesseractOCR $ocr
+ * @property \Xenolope\Quahog\Client $clamav
  */
 trait InjectableProperties
 {
